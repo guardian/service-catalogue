d18dbeeabf83c892ef7621e7757712790cb05b1f
Nicolas Long <nicolas.long@theguardian.com>
2022-08-16 16:31:31 +0100

    Load config

--- go.mod
+++ go.mod
@@ -7,14 +7,18 @@ require (
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.4.4 // indirect
 	github.com/aws/aws-sdk-go-v2/config v1.17.1 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.12.14 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.9.11 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.12 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.18 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.12 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.19 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/v4a v1.0.9 // indirect
 	github.com/aws/aws-sdk-go-v2/service/cloudformation v1.22.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.15.13 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodbstreams v1.13.14 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.5 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.1.13 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.7.12 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.12 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.13.12 // indirect
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.27.5 // indirect
--- main.go
+++ main.go
@@ -5,10 +5,25 @@ import (
 	"encoding/json"
 	"log"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/cloudformation"
 	"github.com/aws/aws-sdk-go-v2/service/cloudformation/types"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	dynamoTypes "github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
+
+type Role struct {
+	OwnerID string
+	Role    string
+}
+
+type Config struct {
+	Config struct {
+		Roles []Role
+	}
+}
 
 type Stack struct {
 	StackName string `json:"stackName"`
@@ -22,13 +37,37 @@ func main() {
 	// store and push to s3 once per day
 	// when requested, return the data
 
+	// load roles from Dynamo
+
 	ctx := context.Background()
 	stacks := []Stack{}
 
 	cfg, err := config.LoadDefaultConfig(ctx, config.WithSharedConfigProfile("deployTools"), config.WithRegion("eu-west-1"))
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("unable to load AWS config: %v", err)
 	}
+
+	dynamodbClient := dynamodb.NewFromConfig(cfg)
+	items, err := dynamodbClient.Query(ctx, &dynamodb.QueryInput{
+		TableName:                 aws.String("config-deploy"),
+		KeyConditionExpression:    aws.String("App = :app AND Stage = :stage"),
+		ExpressionAttributeValues: map[string]dynamoTypes.AttributeValue{":app": &dynamoTypes.AttributeValueMemberS{Value: "cdk-metadata"}, ":stage": &dynamoTypes.AttributeValueMemberS{Value: "INFRA"}},
+	})
+
+	if err != nil {
+		log.Fatalf("unable to load roles from dynamo: %v", err)
+	}
+
+	rolesConfig := Config{}
+	err = attributevalue.UnmarshalMap(items.Items[0], &rolesConfig)
+
+	if err != nil {
+		log.Fatalf("unable to unmarshal roles from dynamo: %v", err)
+	}
+
+	log.Printf("%v", rolesConfig)
+
+	return // Temp
 
 	cfnClient := cloudformation.NewFromConfig(cfg)
 	//s3Client := s3.NewFromConfig(cfg)
