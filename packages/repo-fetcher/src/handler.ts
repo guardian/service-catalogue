a377345ffcf28e45fa7a8356e683f2d0590cf07c
Jon Soul <jonathan.soul@guardian.co.uk>
2022-08-18 16:53:38 +0100

    Save repository data to S3

--- packages/repo-fetcher/src/handler.ts
+++ packages/repo-fetcher/src/handler.ts
@@ -1,5 +1,6 @@
-import config from "./config";
-import { listRepositories, RepositoriesResponse } from "./github";
+import config from "../../common/config";
+import { listRepositories, RepositoriesResponse, RepositoryResponse } from "../../common/github/github";
+import { putItem } from "../../common/aws/s3";
 
 interface Repository {
     id: number,
@@ -7,16 +8,16 @@ interface Repository {
     full_name: string,
     private: boolean,
     description: string | null,
-    created_at: Date,
-    updated_at: Date,
-    pushed_at: Date,
-    size: number,
-    language: string,
-    archived: boolean,
-    open_issues_count: number,
-    is_template: boolean,
-    topics: string[],
-    default_branch: string,
+    created_at: Date | null,
+    updated_at: Date | null,
+    pushed_at: Date | null,
+    size: number | undefined,
+    language: string | null | undefined,
+    archived: boolean | undefined,
+    open_issues_count: number | undefined,
+    is_template: boolean | undefined,
+    topics: string[] | undefined,
+    default_branch: string | undefined,
 }
 
 const parseDateString = (dateString: string | null | undefined): Date | null => {
@@ -26,31 +27,40 @@ const parseDateString = (dateString: string | null | undefined): Date | null => 
     return new Date(dateString);
 }
 
+const transform = (repo: RepositoryResponse): Repository => {
+    return {
+        id: repo.id,
+        name: repo.name,
+        full_name: repo.full_name,
+        private: repo.private,
+        description: repo.description,
+        created_at: parseDateString(repo.created_at),
+        updated_at: parseDateString(repo.updated_at),
+        pushed_at: parseDateString(repo.pushed_at),
+        size: repo.size,
+        language: repo.language,
+        archived: repo.archived,
+        open_issues_count: repo.open_issues_count,
+        is_template: repo.is_template,
+        topics: repo.topics,
+        default_branch: repo.default_branch,
+    }
+}
+
+const save = (repos: Repository[]): Promise<void> => {
+    const key = `${config.dataKeyPrefix}/github/repos.json`
+    return putItem(key, JSON.stringify(repos), config.dataBucketName)
+}
+
 export const main = async (): Promise<void> => {
-    console.log('starting repo-fetcher');
+    console.log('[INFO] starting repo-fetcher');
 
     const reposResponse: RepositoriesResponse = await listRepositories(config);
-    const repos = reposResponse.map(resp => {
-        return {
-            id: resp.id,
-            name: resp.name,
-            full_name: resp.full_name,
-            private: resp.private,
-            description: resp.description,
-            created_at: parseDateString(resp.created_at),
-            updated_at: parseDateString(resp.updated_at),
-            pushed_at: parseDateString(resp.pushed_at),
-            size: resp.size,
-            language: resp.language,
-            archived: resp.archived,
-            open_issues_count: resp.open_issues_count,
-            is_template: resp.is_template,
-            topics: resp.topics,
-            default_branch: resp.default_branch,
-        } as Repository
-    })
+    const repos = reposResponse.map(transform)
 
-    console.log(repos[0]);
+    await save(repos)
+    console.log(`[INFO] found ${repos.length} repos`);
+    console.log(`[INFO] finishing repo-fetcher`);
 }
 
 if (require.main === module) {
