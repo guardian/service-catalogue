fd9f590067646851636201424deea4c7c8a8340d
Nicolas Long <nicolas.long@theguardian.com>
2022-08-22 17:52:58 +0100

    Fix lint errors

--- cdk/lib/cdk-metadata.ts
+++ cdk/lib/cdk-metadata.ts
@@ -1,26 +1,27 @@
-import { join } from 'path';
-import { GuDistributionBucketParameter, GuStackProps, GuAnghammaradTopicParameter } from '@guardian/cdk/lib/constructs/core';
-import { GuScheduledLambda } from '@guardian/cdk/lib/patterns/scheduled-lambda'
+import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
-import { App, Duration } from 'aws-cdk-lib';
+import { GuScheduledLambda } from '@guardian/cdk/lib/patterns/scheduled-lambda';
+import type { App } from 'aws-cdk-lib';
+import { Duration } from 'aws-cdk-lib';
 import { Schedule } from 'aws-cdk-lib/aws-events';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
-import { Bucket } from 'aws-cdk-lib/aws-s3';
 
 export class CdkMetadata extends GuStack {
 	constructor(scope: App, id: string, props: GuStackProps) {
 		super(scope, id, props);
 
-		new GuScheduledLambda(this, "lambda", {
-			app: "TODO",
-			rules: [{
-				schedule: Schedule.rate(Duration.days(1)),
-				description: "Run daily."
-			}],
+		new GuScheduledLambda(this, 'lambda', {
+			app: 'TODO',
+			rules: [
+				{
+					schedule: Schedule.rate(Duration.days(1)),
+					description: 'Run daily.',
+				},
+			],
 			runtime: Runtime.GO_1_X,
-			handler: "main",
-			fileName: "lambda.zip",
-			monitoringConfiguration: { noMonitoring: true } // FIXME
-		})
+			handler: 'main',
+			fileName: 'lambda.zip',
+			monitoringConfiguration: { noMonitoring: true }, // FIXME
+		});
 	}
 }
