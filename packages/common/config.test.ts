a4f1f583cd29b0bf0822d64e0a305926f0d4860b
Robert Kenny <robert.kenny@guardian.co.uk>
2022-09-06 11:51:35 +0100

    fix linting, move tsconfig to root

--- packages/common/config.test.ts
+++ packages/common/config.test.ts
@@ -1,22 +1,21 @@
-import { mandatory, optional } from "./config";
+import { mandatory, optional } from './config';
 
 describe('optional config', function () {
-    it('should return undefined if an item does not exist', function () {
-        expect(optional('DOES_NOT_EXIST')).toBeUndefined()
-    });
-    it('should return an existing item', function () {
-        process.env.CONFIG_EXISTS = 'value'
-        expect(optional('CONFIG_EXISTS')).toBe(process.env.CONFIG_EXISTS);
-    });
+	it('should return undefined if an item does not exist', function () {
+		expect(optional('DOES_NOT_EXIST')).toBeUndefined();
+	});
+	it('should return an existing item', function () {
+		process.env.CONFIG_EXISTS = 'value';
+		expect(optional('CONFIG_EXISTS')).toBe(process.env.CONFIG_EXISTS);
+	});
 });
 
-
 describe('mandatory config', function () {
-    it('should throw an error if an item does not exist', function () {
-        expect(() => mandatory('DOES_NOT_EXIST')).toThrowError()
-    });
-    it('should return an existing item', function () {
-        process.env.CONFIG_EXISTS = 'value'
-        expect(mandatory('CONFIG_EXISTS')).toBe(process.env.CONFIG_EXISTS);
-    });
+	it('should throw an error if an item does not exist', function () {
+		expect(() => mandatory('DOES_NOT_EXIST')).toThrowError();
+	});
+	it('should return an existing item', function () {
+		process.env.CONFIG_EXISTS = 'value';
+		expect(mandatory('CONFIG_EXISTS')).toBe(process.env.CONFIG_EXISTS);
+	});
 });
