63602ace43b7d862782bf42648a8df7a5152dc08
Robert Kenny <robert.kenny@guardian.co.uk>
2022-09-06 14:37:18 +0100

    lint

--- packages/cdk/lib/github-lens.ts
+++ packages/cdk/lib/github-lens.ts
@@ -1,26 +1,25 @@
+import { GuScheduledLambda } from '@guardian/cdk';
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack, GuStringParameter } from '@guardian/cdk/lib/constructs/core';
+import { GuS3Bucket } from '@guardian/cdk/lib/constructs/s3';
 import type { App } from 'aws-cdk-lib';
-import { GuScheduledLambda } from "@guardian/cdk";
-import { Schedule } from "aws-cdk-lib/aws-events";
-import { Runtime } from "aws-cdk-lib/aws-lambda";
-import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
-import { GuS3Bucket } from "@guardian/cdk/lib/constructs/s3";
-import { Duration } from "aws-cdk-lib";
+import { Duration } from 'aws-cdk-lib';
+import { Schedule } from 'aws-cdk-lib/aws-events';
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
 
 export class GithubLens extends GuStack {
 	//TODO: for sharing parameter names between CDK (here) and lambda(s) config code?
-	static parameterNames = {
-
-	}
+	static parameterNames = {};
 	constructor(scope: App, id: string, props: GuStackProps) {
 		super(scope, id, props);
 
-		const app = "github-lens"
+		const app = 'github-lens';
 
 		const dataBucket = new GuS3Bucket(this, `${app}-data-bucket`, {
-			app
-		})
+			app,
+		});
+
 		const dataPutPolicy = new PolicyStatement({
 			effect: Effect.ALLOW,
 			actions: ['s3:PutObject', 's3:PutObjectAcl'],
@@ -28,15 +27,13 @@ export class GithubLens extends GuStack {
 		});
 
 		const repoFetcherApp = 'repo-fetcher';
-		const githubAppId = new GuStringParameter(
-			this,
-			'github-app-id',
-			{
-				description:
-					'(From SSM) The GitHub app ID of the app used to authenticate github-lens',
-				fromSSM: true,
-			},
-		);
+
+		const githubAppId = new GuStringParameter(this, 'github-app-id', {
+			description:
+				'(From SSM) The GitHub app ID of the app used to authenticate github-lens',
+			fromSSM: true,
+		});
+
 		const githubInstallationId = new GuStringParameter(
 			this,
 			'github-installation-id',
@@ -46,16 +43,13 @@ export class GithubLens extends GuStack {
 				fromSSM: true,
 			},
 		);
+
 		//TODO: finisalise KMS decryption of this (add permissions, add decryption to lambda(s) etc.)
-		const githubPrivateKey = new GuStringParameter(
-			this,
-			'github-private-key',
-			{
-				description:
-					'(From SSM) (KMS encrypted) The private key of the app used to authenticate github-lens in the Guardian org',
-				fromSSM: true,
-			},
-		);
+		const githubPrivateKey = new GuStringParameter(this, 'github-private-key', {
+			description:
+				'(From SSM) (KMS encrypted) The private key of the app used to authenticate github-lens in the Guardian org',
+			fromSSM: true,
+		});
 
 		// TODO: double check props
 		new GuScheduledLambda(this, `${repoFetcherApp}-lambda`, {
@@ -66,9 +60,9 @@ export class GithubLens extends GuStack {
 			fileName: `${repoFetcherApp}.zip`,
 			monitoringConfiguration: {
 				toleratedErrorPercentage: 0,
-				snsTopicName: "devx-alerts",
+				snsTopicName: 'devx-alerts',
 			},
-			rules: [{ schedule: Schedule.expression("cron(0 8 ? * * *)") }],
+			rules: [{ schedule: Schedule.expression('cron(0 8 ? * * *)') }],
 			timeout: Duration.seconds(300),
 			environment: {
 				STAGE: this.stage, //TODO: finish environment definition
