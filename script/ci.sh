41c3cffd4a21bec2e9253599a7f9f2303cc90b4c
Robert Kenny <robert.kenny@guardian.co.uk>
2022-09-06 15:59:08 +0100

    build and deploy

---
+++ script/ci.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+ROOT_DIR="${DIR}/.."
+
+npm ci
+npm run typecheck
+npm run lint
+npm run test
+npm run synth
+npm run build
+
+get_abs_filename() {
+  # $1 : relative filename
+  echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
+}
+
+# Create zip files for guardian/actions-riff-raff@v1 GHA
+for file in "${ROOT_DIR}/packages/*/dist/handler.js"
+do
+	filename=$(get_abs_filename $file)
+    directory=$(dirname "$filename")
+	handler_name=$(basename "$filename" .js)    
+
+    zip -FSjr "${directory}/${handler_name}.zip" "$filename"
+done
