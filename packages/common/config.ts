80494601dcf41bf72c364e854e13a290ca4771ee
Natasha <natasha.thrale@guardian.co.uk>
2022-09-16 09:57:15 +0100

    make the ci check happy

--- packages/common/config.ts
+++ packages/common/config.ts
@@ -3,6 +3,7 @@ import * as dotenv from 'dotenv';
 dotenv.config({ path: `${__dirname}/../../.env` });
 
 export type Config = {
+	dataKeyPrefix: string;
 	github: {
 		appId: string;
 		appPrivateKey: string;
@@ -23,7 +24,7 @@ export const optional = (item: string): string | undefined => process.env[item];
 export const optionalWithDefault = (item: string, _default: string): string =>
 	optional(item) ?? _default;
 
-export const config = {
+export const config: Config = {
 	github: {
 		appId: mandatory('GITHUB_APP_ID'),
 		appPrivateKey: mandatory('GITHUB_APP_PRIVATE_KEY'),
