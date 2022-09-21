9492ca7c6db5f3cb8adf34b97e5fc613efce5844
Robert Kenny <kenoir@gmail.com>
2022-09-21 12:45:26 +0100

    Setup jest in order that tests in common package run

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,18 @@
-const generateProject = name => {
-  return {
-    displayName: name,
-    transform: {
-      '^.+\\.tsx?$': 'ts-jest',
-    },
-    testMatch: [`<rootDir>/packages/${name}/**/*.test.ts`],
-    setupFilesAfterEnv: [`./packages/${name}/jest.setup.js`],
-  }
-}
-
-module.exports = {
-  verbose: true,
-  testEnvironment: 'node',
-  projects: ['cdk', 'api', 'repo-fetcher', 'teams-fetcher'].map(generateProject)
+const generateProject = (name) => {
+	return {
+		displayName: name,
+		transform: {
+			'^.+\\.tsx?$': 'ts-jest',
+		},
+		testMatch: [`<rootDir>/packages/${name}/**/*.test.ts`],
+		setupFilesAfterEnv: [`./packages/${name}/jest.setup.js`],
+	};
 };
 
+module.exports = {
+	verbose: true,
+	testEnvironment: 'node',
+	projects: ['cdk', 'api', 'repo-fetcher', 'teams-fetcher', 'common'].map(
+		generateProject,
+	),
+};
