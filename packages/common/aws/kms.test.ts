a935b5d9a2c61ca9f1e3ef3089ca0e838ac36350
Robert Kenny <kenoir@gmail.com>
2022-09-21 15:06:45 +0100

    add aws mocking lib

--- packages/common/aws/kms.test.ts
+++ packages/common/aws/kms.test.ts
@@ -1,5 +1,21 @@
+import { DecryptCommand, KMSClient } from '@aws-sdk/client-kms';
+import { mockClient } from 'aws-sdk-client-mock';
+import { decrypt } from './kms';
+
+const kmsMock = mockClient(KMSClient);
+
+beforeEach(() => {
+	kmsMock.reset();
+});
+
 describe('test', function () {
-	it('should fail', function () {
-		expect(true).toBe(false);
+	it('should fail', async function () {
+		kmsMock.on(DecryptCommand).resolves({
+			Plaintext: Buffer.from('boo', 'base64'),
+		});
+
+		const bat = await decrypt('foo', 'bar');
+
+		expect(bat).toBe(false);
 	});
 });
