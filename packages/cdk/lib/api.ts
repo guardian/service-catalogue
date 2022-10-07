fcc9aa1b29a49c511b6590e1abc14cd9d62d5738
Robert Kenny <kenoir@gmail.com>
2022-10-07 13:32:54 +0100

    rename api definition

--- packages/cdk/lib/api.ts
+++ packages/cdk/lib/api.ts
@@ -9,7 +9,9 @@ export class GithubLensApi extends Stack {
 	constructor(scope: App, id: string, props: GuStackProps) {
 		super(scope, id, props);
 
-		const openApiYaml = fs.readFileSync('../api/src/openApi.yaml').toString();
+		const openApiYaml = fs
+			.readFileSync('../api/src/api-definition.yaml')
+			.toString();
 		const yamlString = yaml.load(openApiYaml);
 
 		new SpecRestApi(this, 'lens-api', {
