7a37d310dd700b86e191c7a09b144d49ab77924f
Julia Branke <Julia.Branke@guardian.co.uk>
2022-10-13 15:29:36 +0100

    ran eslint

--- packages/cdk/bin/cdk.ts
+++ packages/cdk/bin/cdk.ts
@@ -1,9 +1,12 @@
 import 'source-map-support/register';
+import { GuRootExperimental } from '@guardian/cdk/lib/experimental/constructs/root';
 import { GithubLens } from '../lib/github-lens';
-
-import { GuRootExperimental } from "@guardian/cdk/lib/experimental/constructs/root";
 
 const app = new GuRootExperimental();
 
-new GithubLens(app, "GithubLens-INFRA", {stack: 'deploy', stage: 'INFRA', env:{region: "eu-west-1"} });
+new GithubLens(app, 'GithubLens-INFRA', {
+	stack: 'deploy',
+	stage: 'INFRA',
+	env: { region: 'eu-west-1' },
+});
 //new GithubLens(app, "my-stack-PROD", {stack: 'deploy', stage: 'PROD'});
