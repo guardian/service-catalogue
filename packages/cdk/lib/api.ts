d734ebab4dcc6a4371a2acab98aca739a0a1a8fb
Robert Kenny <kenoir@gmail.com>
2022-10-14 13:18:37 +0100

    update and lint

--- packages/cdk/lib/api.ts
+++ packages/cdk/lib/api.ts
@@ -1,24 +1,23 @@
+import * as fs from 'fs';
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import type { App } from 'aws-cdk-lib';
-import {Stack} from "aws-cdk-lib";
-import {ApiDefinition, SpecRestApi} from "aws-cdk-lib/aws-apigateway";
-import * as fs from "fs";
-import yaml from "js-yaml"
-
+import { Stack } from 'aws-cdk-lib';
+import { ApiDefinition, SpecRestApi } from 'aws-cdk-lib/aws-apigateway';
+import yaml from 'js-yaml';
 
 export class GithubLensApi extends Stack {
-    constructor(scope: App, id: string, props: GuStackProps) {
+	constructor(scope: App, id: string, props: GuStackProps) {
+		super(scope, id, props);
 
-        super(scope, id, props);
+		//this constant also exists in github-lens.ts and should probably be pulled out.
+		const app = 'github-lens';
 
-        //this constant also exists in github-lens.ts and should probably be pulled out.
-        const app = 'github-lens';
+		const yamlString = yaml.load(
+			fs.readFileSync('../api/src/openapi.yaml').toString(),
+		);
 
-        const yamlString = yaml.load(fs.readFileSync('../api/src/openapi.yaml').toString())
-
-        const api = new SpecRestApi(this, 'lens-api', {
-            apiDefinition: ApiDefinition.fromInline(yamlString)
-        });
-
-    }
+		const api = new SpecRestApi(this, 'lens-api', {
+			apiDefinition: ApiDefinition.fromInline(yamlString),
+		});
+	}
 }
