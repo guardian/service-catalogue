d5725a0624dd77e2ae4f8725094b5e71eb15b272
Akash Askoolum <akash1810@gmail.com>
2022-10-26 09:11:54 +0100

    chore: Remove cdk-metadata-roles CFN stack
    We've moved to provision this role within aws-account-setup.
    
    See https://github.com/guardian/aws-account-setup/pull/43

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,17 +1,6 @@
 import 'source-map-support/register';
 import { App } from 'aws-cdk-lib';
 import { CdkMetadata } from '../lib/cdk-metadata';
-import { CdkMetadataAccess } from '../lib/roles';
 
 const app = new App();
 new CdkMetadata(app, 'CdkMetadata-INFRA', { stack: 'deploy', stage: 'INFRA' });
-new CdkMetadataAccess(app, 'CdkMetadataAccess-INFRA', {
-	// TODO at the moment we treat this as a placeholder and use a Stack
-	// parameter (which RR automatically populates) to handle multiple stacks.
-	// Once @actions-riff-raff supports contentDirectory
-	// (https://github.com/guardian/actions-riff-raff/issues/13) we should use
-	// GuRootExperimental instead here and drop the Stack parameter in favour of
-	// generating a template per stack.
-	stack: 'PLACEHOLDER',
-	stage: 'INFRA',
-});
