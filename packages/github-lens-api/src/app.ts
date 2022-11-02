7cebe389342b1f8d148cf7dda42cebfa0155eeb3
Robert Kenny <kenoir@gmail.com>
2022-11-02 14:13:51 +0000

    Add router describing request handler

--- packages/github-lens-api/src/app.ts
+++ packages/github-lens-api/src/app.ts
@@ -1,32 +1,38 @@
 import { json as jsonBodyParser } from 'body-parser';
 import type { Repository } from 'common/github/github';
 import cors from 'cors';
-import express from 'express';
 import type { Express } from 'express';
+import express, { Router } from 'express';
 import asyncHandler from 'express-async-handler';
+import { getDescribeRouterHandler } from './expressRoutes';
 
 export function buildApp(repoData: Promise<Repository[]>): Express {
 	const app = express();
+	const router = Router();
 
-	app.use(jsonBodyParser());
+	router.use(jsonBodyParser());
 
-	app.use(
+	router.use(
 		cors({
 			origin: /\.(dev-)?gutools.co.uk$/,
 			credentials: true,
 		}),
 	);
 
-	app.get('/healthcheck', (req: express.Request, res: express.Response) => {
+	router.get('/', getDescribeRouterHandler(router));
+
+	router.get('/healthcheck', (req: express.Request, res: express.Response) => {
 		res.status(200).json({ status: 'OK', stage: 'INFRA' });
 	});
 
-	app.get(
+	router.get(
 		'/repos',
 		asyncHandler(async (req: express.Request, res: express.Response) => {
 			res.status(200).json(await repoData);
 		}),
 	);
+
+	app.use('/', router);
 
 	return app;
 }
