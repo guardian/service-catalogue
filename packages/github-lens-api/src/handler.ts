68c0607b92a02c23c7317582e9a8c61428f2d714
Nicolas Long <nicolas.long@theguardian.com>
2022-11-08 13:45:03 +0000

    Make API private by moving to ALB/EC2
    Security Groups (network restrictions) are used to limit API access
    to within the VPC and peered Guardian networks - specifically the
    VPN.
    
    API Gateway can be used to achieve this too but it is difficult to
    use a custom domain when doing so. See e.g.
    
    https://georgemao.medium.com/enabling-private-apis-with-custom-domain-names-aws-api-gateway-df1b62b0ba7c
    
    which requires an ALB for this.
    
    If, as it seems, an ALB is required anyway, then using API Gateway
    seems to only add complexity here.
    
    The shift to EC2 is not strictly speaking required but provides the
    following:
    
    * consistency with Cloudformation Lens (nee cdk-metadata)
    * ability to use our CDK GuEc2App pattern
    * a larger response payload (lambda behind ALB is limited to 1mb)
    
    The cost impact of switching to EC2 here is $7/month for a single
    t4g.nano instance (not reserved).
    
    Longer-term it may make sense to move to a monorepo and share CDK
    and ALB with Cloudformation Lens and the Services Catalogue itself.

--- packages/github-lens-api/src/handler.ts
+++ packages/github-lens-api/src/handler.ts
@@ -1,5 +1,4 @@
 import path from 'path';
-import serverlessExpress from '@vendia/serverless-express';
 import { getObject, getS3Client } from 'common/aws/s3';
 import type { Repository } from 'common/github/github';
 import { configureLogging } from 'common/log/log';
@@ -23,12 +22,8 @@ const repoData = getObject<Repository[]>(
 
 const app = buildApp(repoData);
 
-if (process.env.LOCAL === 'true') {
-	const PORT = 3232;
-	app.listen(PORT, () => {
-		console.log(`Listening on port ${PORT}`);
-		console.log(`Access via http://localhost:${PORT}`);
-	});
-}
-
-export const main = serverlessExpress({ app });
+const PORT = process.env.PORT ?? '3232';
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`);
+	console.log(`Access via http://localhost:${PORT}`);
+});
