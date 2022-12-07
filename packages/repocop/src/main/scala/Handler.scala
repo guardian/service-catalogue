85865b112b78410fdab06654a423fea16c0132f4
Natasha <natasha.thrale@guardian.co.uk>
2022-12-07 11:48:26 +0000

    show result in lambda output

--- packages/repocop/src/main/scala/Handler.scala
+++ packages/repocop/src/main/scala/Handler.scala
@@ -1,5 +1,10 @@
 package com.gu.repocop
 
 class Handler {
-  def handler: Either[Throwable, List[EvaluatedRepo]] = GHLensAPI.getRepos.map(Rules.evaluateReposForTeam(_, "devx-operations"))
+  def handler: String = {
+    val result: Either[Throwable, List[EvaluatedRepo]] = GHLensAPI.getRepos.map(Rules.evaluateReposForTeam(_, "devx-operations"))
+    result match
+      case Left(e) => e.getMessage
+      case Right(repos) => s"evaluated ${repos.length.toString} repos"
+  }
 }
