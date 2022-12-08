1b13028f3458f3d48cf5363875599faeac2d9425
Natasha <natasha.thrale@guardian.co.uk>
2022-12-08 10:42:25 +0000

    configure repocop vpc

--- packages/cdk/lib/repocop.ts
+++ packages/cdk/lib/repocop.ts
@@ -1,5 +1,6 @@
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
+import { GuVpc, SubnetType } from '@guardian/cdk/lib/constructs/ec2';
 import { GuScheduledLambda } from '@guardian/cdk/lib/patterns/scheduled-lambda';
 import type { App } from 'aws-cdk-lib';
 import { Duration } from 'aws-cdk-lib';
@@ -22,6 +23,13 @@ export class Repocop extends GuStack {
 			},
 			rules: [{ schedule: Schedule.cron({ minute: '0', hour: '8' }) }],
 			timeout: Duration.seconds(300),
+			vpc: GuVpc.fromIdParameter(this, 'deployToolsVPC'),
+			vpcSubnets: {
+				subnets: GuVpc.subnetsFromParameter(this, {
+					type: SubnetType.PRIVATE,
+					app: repoCopApp,
+				}),
+			},
 		});
 	}
 }
