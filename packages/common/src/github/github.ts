41f48aeed821367c4dca1de4e4612f78b5577c7e
Julia Branke <Julia.Branke@guardian.co.uk>
2022-12-16 16:48:05 +0000

    Remove accidential esling-disable

--- packages/common/src/github/github.ts
+++ packages/common/src/github/github.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { createAppAuth } from '@octokit/auth-app';
 import { throttling } from '@octokit/plugin-throttling';
 import { Octokit } from '@octokit/rest';
