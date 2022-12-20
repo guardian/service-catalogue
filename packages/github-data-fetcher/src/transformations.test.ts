e47bea99a008f71e40549c8c805ce350300d2a83
Natasha <natasha.thrale@guardian.co.uk>
2022-12-20 14:14:15 +0000

    verify asRepo creates the correct fields

--- packages/github-data-fetcher/src/transformations.test.ts
+++ packages/github-data-fetcher/src/transformations.test.ts
@@ -32,4 +32,20 @@ describe('repository objects', function () {
 		expect(finalRepoObject.owners).toStrictEqual(['team3', 'team4']);
 		expect(finalRepoObject.name).toStrictEqual('repo-name');
 	});
+	it('should pass through the correct RepositoryResponse fields', function () {
+		const repoResponse: RepositoryResponse = mockRepo;
+		const actual = asRepo(repoResponse, [], []);
+		expect(actual.name).toStrictEqual('repo-name');
+		expect(actual.pushed_at).toStrictEqual('2011-01-26T19:06:43Z');
+		expect(actual.updated_at).toStrictEqual('2011-01-26T19:14:43Z');
+		expect(actual.created_at).toStrictEqual('2011-01-26T19:01:12Z');
+		expect(actual.size).toStrictEqual(108);
+		expect(actual.owners).toStrictEqual([]);
+		expect(actual.archived).toStrictEqual(false);
+		expect(actual.languages).toStrictEqual([]);
+		expect(actual.topics).toStrictEqual(['production']);
+		expect(actual.default_branch).toStrictEqual('master');
+		expect(actual.lastCommit).toStrictEqual(undefined);
+		expect(actual.private).toStrictEqual(false);
+	});
 });
