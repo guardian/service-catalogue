63427aa5eebe1cb2b5095084cad4acae3c75808c
Natasha <67543397+NovemberTang@users.noreply.github.com>
2023-03-15 14:52:57 +0000

    add some more tests

--- packages/github-data-fetcher/src/transformations.test.ts
+++ packages/github-data-fetcher/src/transformations.test.ts
@@ -1,8 +1,12 @@
 import type { RepositoryResponse } from 'common/github/github';
-import type { Repository } from 'common/model/github';
+import type { Commit, Repository } from 'common/model/github';
 import { mockRepo } from './mockRepo';
 import type { RepoAndOwner } from './transformations';
 import { asRepo, findOwnersOfRepo } from './transformations';
+
+const emptyCommit: Commit = {
+	message: '',
+};
 
 describe('repository owners', function () {
 	it('should not be returned if none exist for that repo', function () {
@@ -31,5 +35,38 @@ describe('repository objects', function () {
 
 		expect(finalRepoObject.owners).toStrictEqual(['team3', 'team4']);
 		expect(finalRepoObject.name).toStrictEqual('repo-name');
+	});
+	it('should combine a RepositoryResponse with the most recent commit', function () {
+		const commit = { ...emptyCommit, message: 'hello' };
+		const repo: RepositoryResponse = mockRepo;
+		const finalRepoObject: Repository = asRepo(repo, [], [], commit);
+
+		expect(finalRepoObject.last_commit?.message).toStrictEqual('hello');
+		expect(finalRepoObject.name).toStrictEqual('repo-name');
+	});
+});
+
+describe('date strings', function () {
+	const repo = { ...mockRepo, updated_at: '2015-01-25T08:09:10Z' };
+	it('should convert to date objects if they exist', function () {
+		const actualDate = asRepo(repo, [], [], emptyCommit).updated_at;
+		const expectedDate = new Date(2015, 0, 25, 8, 9, 10);
+		expect(actualDate).toEqual(expectedDate);
+	});
+	it('should return a null if there is no date', function () {
+		const actualDate = asRepo(
+			{ ...mockRepo, updated_at: '' },
+			[],
+			[],
+			emptyCommit,
+		).updated_at;
+		expect(actualDate).toEqual(null);
+		const actualDate2 = asRepo(
+			{ ...mockRepo, updated_at: undefined },
+			[],
+			[],
+			emptyCommit,
+		).updated_at;
+		expect(actualDate2).toEqual(null);
 	});
 });
