7ecb9e37898927c1f4cd15c18b95809ae32a0d00
akash1810 <akash1810@users.noreply.github.com>
2023-06-15 09:16:19 +0100

    chore(deps): Bump the version of CloudQuery CLI, sources, and destination

--- packages/cdk/lib/ecs/versions.ts
+++ packages/cdk/lib/ecs/versions.ts
@@ -4,35 +4,35 @@ export const Versions = {
 	 *
 	 * @see https://github.com/cloudquery/cloudquery/releases?q=cli
 	 */
-	CloudqueryCli: '3.5.0',
+	CloudqueryCli: '3.5.2',
 
 	/**
 	 * The version of the CloudQuery Postgres destination plugin to install.
 	 *
 	 * @see https://github.com/cloudquery/cloudquery/releases?q=plugins-destination-postgresql
 	 */
-	CloudqueryPostgres: '4.2.0',
+	CloudqueryPostgres: '4.2.2',
 
 	/**
 	 * The version of the CloudQuery AWS source plugin to install.
 	 *
 	 * @see https://github.com/cloudquery/cloudquery/releases?q=plugins-source-aws
 	 */
-	CloudqueryAws: '17.4.0',
+	CloudqueryAws: '18.3.0',
 
 	/**
 	 * The version of the CloudQuery GitHub source plugin to install.
 	 *
 	 * @see https://github.com/cloudquery/cloudquery/releases?q=plugins-source-github
 	 */
-	CloudqueryGithub: '5.2.0',
+	CloudqueryGithub: '6.0.2',
 
 	/**
 	 * The version of the CloudQuery Fastly source plugin to install.
 	 *
 	 * @see https://github.com/cloudquery/cloudquery/releases?q=plugins-source-fastly
 	 */
-	CloudqueryFastly: '1.3.1',
+	CloudqueryFastly: '2.0.2',
 
 	/**
 	 * The version of the our custom Galaxies source plugin to install.
@@ -46,7 +46,7 @@ export const Versions = {
 	 *
 	 * @see https://github.com/cloudquery/cloudquery/releases?q=plugins-source-snyk
 	 */
-	CloudquerySnyk: '3.0.0',
+	CloudquerySnyk: '3.0.2',
 
 	/**
 	 * The version of the CloudQuery Snyk source plugin to install.
