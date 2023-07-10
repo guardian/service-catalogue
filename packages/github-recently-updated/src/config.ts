5b9424c2805105c52ffbfbce96ef8773f1ca8ef4
akash1810 <akash1810@users.noreply.github.com>
2023-07-10 22:24:58 +0100

    feat: Implement database query

--- packages/github-recently-updated/src/config.ts
+++ packages/github-recently-updated/src/config.ts
@@ -12,4 +12,10 @@ export const config = {
 	app: environmentOrError('APP'),
 	stage: environmentOrError('STAGE'),
 	logLevel: getLogLevel(process.env.LOG_LEVEL),
+	differenceInDays: environmentOrError('DIFFERENCE_IN_DAYS'),
+	database: {
+		host: environmentOrError('DATABASE_HOST'),
+		user: environmentOrError('DATABASE_USER'),
+		password: environmentOrError('DATABASE_PASSWORD'),
+	},
 };
--- packages/github-recently-updated/src/handler.ts
+++ packages/github-recently-updated/src/handler.ts
@@ -1,14 +1,37 @@
+import { Client } from 'ts-postgres';
 import { config } from './config';
 import { configureLogging } from './logger';
 
 export const main = async () => {
-	const { logLevel, app, stage } = config;
+	const {
+		logLevel,
+		database: { host, user, password },
+		differenceInDays,
+	} = config;
 
 	// Configure the logger first, so any calls to `console.log`, `console.warn` etc. are filtered as necessary.
 	configureLogging(logLevel);
 
-	console.log(`Hello from ${app} ${stage}!`);
+	const client = new Client({ host, user, password });
 
-	console.debug(`Sleeping for 1ms`);
-	await new Promise((r) => setTimeout(r, 1));
+	try {
+		await client.connect();
+		console.log(`Connected to database`);
+
+		const result = await client.query(
+			`SELECT full_name FROM github_repositories WHERE GREATEST(pushed_at, updated_at) > NOW() - INTERVAL '${differenceInDays} days'`,
+		);
+
+		const repositories = [];
+		for await (const row of result) {
+			repositories.push(row.get('full_name'));
+		}
+
+		console.log(
+			`Repositories updated in the last ${differenceInDays} days: ${repositories.length}`,
+		);
+		console.debug(repositories.join(','));
+	} finally {
+		await client.end();
+	}
 };
