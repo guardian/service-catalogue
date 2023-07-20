ce6ec678ec671d991396fe1136b4a3040953f0cc
akash1810 <akash1810@users.noreply.github.com>
2023-07-20 08:00:54 +0100

    chore: Simplify Jest config
    Remove `generateProject` as we now only have one project,
    so no need for the abstraction.

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,16 @@
-const generateProject = (name) => {
-	return {
-		displayName: name,
-		transform: {
-			'^.+\\.tsx?$': 'ts-jest',
-		},
-		transformIgnorePatterns: [
-			'node_modules/(?!@guardian/private-infrastructure-config)',
-		],
-		testMatch: [`<rootDir>/packages/${name}/**/*.test.ts`],
-		setupFilesAfterEnv: [`./packages/${name}/jest.setup.js`],
-		moduleNameMapper: {
-			'^common$': '<rootDir>/packages/common/src',
-			'^common/(.*)$': '<rootDir>/packages/common/src/$1',
-		},
-	};
-};
-
 module.exports = {
 	verbose: true,
 	testEnvironment: 'node',
 	projects: [
-		'cdk',
-	].map(generateProject),
+		{
+			displayName: 'cdk',
+			transform: {
+				'^.+\\.tsx?$': 'ts-jest',
+			},
+			transformIgnorePatterns: [
+				'node_modules/(?!@guardian/private-infrastructure-config)',
+			],
+			setupFilesAfterEnv: [`<rootDir>/packages/cdk/jest.setup.js`],
+		},
+	],
 };
