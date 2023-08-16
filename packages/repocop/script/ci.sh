933ff7ee5b8b74cbdf9a2fe2597461b41e0949a6
akash1810 <akash1810@users.noreply.github.com>
2023-08-16 18:53:37 +0100

    ci(repocop): Update CI to run tests and create a smaller PROD artifact
    This change:
      - Splits out dev requirements from prod requirements
      - Creates a zip file only including prod requirements
    
    The result is an inflated zip file that is ~153MB, which is under the 250MB limit of AWS lambda.

--- packages/repocop/script/ci.sh
+++ packages/repocop/script/ci.sh
@@ -2,21 +2,20 @@
 
 set -e
 
+MAJOR_PYTHON_VERSION="3.11"
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 REPOCOP_DIR="$(realpath "${DIR}/..")"
+
+VENV_DIR="${REPOCOP_DIR}/.venv"
+
 APP_NAME="repocop"
+ZIP_FILE="${APP_NAME}.zip"
 
-python3 -m venv "$REPOCOP_DIR/.venv"
-source "$REPOCOP_DIR/.venv/bin/activate"
-pip3 install -r "$REPOCOP_DIR/requirements.txt"
+python3 -m venv "$VENV_DIR"
+source "$VENV_DIR/bin/activate"
 
-( #Steps to package the python venv
-  MAJOR_PYTHON_VERSION="3.11"
-  PACKAGE_DIR="${REPOCOP_DIR}/.venv/lib/python${MAJOR_PYTHON_VERSION}/site-packages"
-  ZIP_FILE="${APP_NAME}.zip"
-
-  cp "${REPOCOP_DIR}/"*.py "${PACKAGE_DIR}"
-  cd "${PACKAGE_DIR}"
-  zip -FSr "${REPOCOP_DIR}/${ZIP_FILE}" .
-)
+# Install dependencies into "target" directory, copy our code there too, then zip the whole thing.
+pip3 install -r "$REPOCOP_DIR/requirements.txt" -t target
+cp "${REPOCOP_DIR}/"*.py target
+( cd target && zip -FSr "${REPOCOP_DIR}/${ZIP_FILE}" . )
--- packages/repocop/script/test.sh
+++ packages/repocop/script/test.sh
@@ -5,8 +5,9 @@ set -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 REPOCOP_DIR="$(realpath "${DIR}/..")"
 
-python3 -m venv "$REPOCOP_DIR/.venv"
-source "$REPOCOP_DIR/.venv/bin/activate"
-pip3 install -r "$REPOCOP_DIR/requirements.txt"
-pytest
-deactivate
+VENV_DIR="${REPOCOP_DIR}/.venv"
+
+python3 -m venv "$VENV_DIR"
+source "$VENV_DIR/bin/activate"
+pip3 install -r "$REPOCOP_DIR/requirements-dev.txt"
+python3 -m pytest
