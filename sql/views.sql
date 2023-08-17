f214da9f28b25403f9eaacbfb5ce9a8460009587
Natasha <67543397+NovemberTang@users.noreply.github.com>
2023-08-17 09:16:13 +0100

    include archived repos in view_repo_ownership (this view is now in prod)

--- sql/views.sql
+++ sql/views.sql
@@ -60,8 +60,7 @@ select ght.id        as "github_team_id"
 from github_team_repositories tr
          join github_teams ght on tr.team_id = ght.id
          left join galaxies_teams_table gtt on ght.slug = gtt.team_primary_github_team
-where tr.role_name = 'admin'
-  and tr.archived = false;
+where tr.role_name = 'admin';
 
 CREATE OR REPLACE FUNCTION coalesce_dates(image_creation_time text, instance_launch_time timestamp) RETURNS date
     LANGUAGE SQL
