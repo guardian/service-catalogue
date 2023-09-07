399a889e1c2d0485530a4f9c48e6eb141520f889
akash1810 <akash1810@users.noreply.github.com>
2023-09-07 17:18:55 +0100

    fix: Grant the repocop DB user permission to delete and insert into the `repocop_github_repository_rules` table
    This statement currently needs to be manually applied.

--- sql/dbuser.sql
+++ sql/dbuser.sql
@@ -2,3 +2,7 @@ CREATE USER repocop WITH LOGIN;
 GRANT USAGE ON SCHEMA public to repocop;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO repocop;
 GRANT rds_iam TO repocop;
+
+-- This table is created via a Prisma migration
+-- TODO what order should all the SQL be applied now?
+GRANT ALL ON public.repocop_github_repository_rules TO repocop;
