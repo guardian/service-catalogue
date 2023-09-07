fb21c21bdc40585052e911e2406d6e3b03c158cb
Julia Branke <Julia.Branke@guardian.co.uk>
2023-09-07 11:57:50 +0100

    Add table repocop_github_repository_rules with prisma migrate
    Co-authored-by: Akash <akash1810@users.noreply.github.com>

---
+++ packages/repocop/prisma/migrations/20230907105726_create_table_repocop_github_repository_rules/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "repocop_github_repository_rules" (
+    "full_name" TEXT NOT NULL,
+    "repository_01" BOOLEAN NOT NULL,
+    "repository_02" BOOLEAN NOT NULL,
+    "repository_03" BOOLEAN NOT NULL,
+    "repository_04" BOOLEAN NOT NULL,
+    "repository_05" BOOLEAN NOT NULL,
+    "repository_06" BOOLEAN NOT NULL,
+    "repository_07" BOOLEAN NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "repocop_github_repository_rules_full_name_key" ON "repocop_github_repository_rules"("full_name");
