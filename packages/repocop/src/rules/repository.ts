3eaaeede19da9d192f42e934cafd865b4d0f1cde
akash1810 <akash1810@users.noreply.github.com>
2023-09-11 09:35:04 +0100

    docs(repocop): Document the repository rules a little

--- packages/repocop/src/rules/repository.ts
+++ packages/repocop/src/rules/repository.ts
@@ -4,10 +4,18 @@ import type {
 	repocop_github_repository_rules,
 } from '@prisma/client';
 
+/**
+ * Apply the following rule to a GitHub repository:
+ *   > The default branch name should be "main".
+ */
 export function repository01(repo: github_repositories): boolean {
 	return repo.default_branch === 'main';
 }
 
+/**
+ * Apply the following rule to a GitHub repository:
+ *   > Enable branch protection for the default branch, ensuring changes are reviewed before being deployed.
+ */
 export function repository02(
 	repo: github_repositories,
 	branches: github_repository_branches[],
@@ -23,6 +31,9 @@ export function repository02(
 	}
 }
 
+/**
+ * Apply rules to a repository as defined in https://github.com/guardian/recommendations/blob/main/best-practices.md.
+ */
 export function repositoryRuleEvaluation(
 	repo: github_repositories,
 	allBranches: github_repository_branches[],
