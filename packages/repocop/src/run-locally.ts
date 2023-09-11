c6ec6c2b29675f80c4880603ee5689253944b413
akash1810 <akash1810@users.noreply.github.com>
2023-09-11 09:00:03 +0100

    chore(repocop): Run `eslint --fix`

--- packages/repocop/src/run-locally.ts
+++ packages/repocop/src/run-locally.ts
@@ -1,12 +1,12 @@
 import { main } from './index';
 
 if (require.main === module) {
-    // TODO set these values from a .env file.
-    //  The .env file should also be used within `docker-compose.yml` to stay DRY.
-    process.env.STAGE = 'DEV';
-    process.env.DATABASE_HOSTNAME = 'localhost';
-    process.env.DATABASE_PASSWORD = 'not_at_all_secret';
-    process.env.DATABASE_USER = 'postgres'
+	// TODO set these values from a .env file.
+	//  The .env file should also be used within `docker-compose.yml` to stay DRY.
+	process.env.STAGE = 'DEV';
+	process.env.DATABASE_HOSTNAME = 'localhost';
+	process.env.DATABASE_PASSWORD = 'not_at_all_secret';
+	process.env.DATABASE_USER = 'postgres';
 
-    void (async () => await main())();
+	void (async () => await main())();
 }
