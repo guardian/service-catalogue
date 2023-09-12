4ff435a1fb2a91a04b215b27321c1162c9870f3c
akash1810 <akash1810@users.noreply.github.com>
2023-09-12 21:26:31 +0100

    feat(repocop): Implement rule repository_04
    Implements rule repository_04:
    
    > Grant at least one GitHub team Admin access - typically, the dev team that own the project.
    > Repositories without one of the following topics are exempt: production, testing, documentation.

--- packages/repocop/src/index.ts
+++ packages/repocop/src/index.ts
@@ -1,6 +1,7 @@
 import type {
 	github_repositories,
 	github_repository_branches,
+	github_team_repositories,
 	repocop_github_repository_rules,
 } from '@prisma/client';
 import { PrismaClient } from '@prisma/client';
@@ -50,6 +51,26 @@ async function getRepositoryBranches(
 	return branches;
 }
 
+async function getRepositoryTeams(
+	client: PrismaClient,
+	repository: github_repositories,
+): Promise<github_team_repositories[]> {
+	const data = await client.github_team_repositories.findMany({
+		where: {
+			id: repository.id,
+		},
+	});
+
+	// `full_name` is typed as nullable, in reality it is not, so the fallback to `id` shouldn't happen
+	const repoIdentifier = repository.full_name ?? repository.id;
+
+	console.log(
+		`Found ${data.length} teams with access to repository ${repoIdentifier}`,
+	);
+
+	return data;
+}
+
 async function evaluateRepositories(
 	client: PrismaClient,
 	ignoredRepositoryPrefixes: string[],
@@ -59,7 +80,8 @@ async function evaluateRepositories(
 	return await Promise.all(
 		repositories.map(async (repo) => {
 			const branches = await getRepositoryBranches(client, repo);
-			return repositoryRuleEvaluation(repo, branches);
+			const teams = await getRepositoryTeams(client, repo);
+			return repositoryRuleEvaluation(repo, branches, teams);
 		}),
 	);
 }
--- packages/repocop/src/rules/repository.ts
+++ packages/repocop/src/rules/repository.ts
@@ -1,6 +1,7 @@
 import type {
 	github_repositories,
 	github_repository_branches,
+	github_team_repositories,
 	repocop_github_repository_rules,
 } from '@prisma/client';
 
@@ -32,11 +33,35 @@ export function repository02(
 }
 
 /**
+ * Apply the following rule to a GitHub repository:
+ *   > Grant at least one GitHub team Admin access - typically, the dev team that own the project.
+ *   > Repositories without one of the following topics are exempt: production, testing, documentation.
+ */
+export function repository04(
+	repo: github_repositories,
+	teams: github_team_repositories[],
+): boolean {
+	const adminTeams = teams.filter(
+		({ id, role_name }) => id === repo.id && role_name === 'admin',
+	);
+	const hasAdminTeam = adminTeams.length > 0;
+
+	// only evaluate repositories with no topic or a relevant topic
+	const relevantTopics = ['production', 'testing', 'documentation'];
+	const hasRelevantTopic =
+		repo.topics.length === 0 ||
+		repo.topics.filter((topic) => relevantTopics.includes(topic)).length > 0;
+
+	return hasAdminTeam && hasRelevantTopic;
+}
+
+/**
  * Apply rules to a repository as defined in https://github.com/guardian/recommendations/blob/main/best-practices.md.
  */
 export function repositoryRuleEvaluation(
 	repo: github_repositories,
 	allBranches: github_repository_branches[],
+	teams: github_team_repositories[],
 ): repocop_github_repository_rules {
 	return {
 		full_name: repo.full_name!,
@@ -45,7 +70,7 @@ export function repositoryRuleEvaluation(
 
 		// TODO - implement these rules
 		repository_03: false,
-		repository_04: false,
+		repository_04: repository04(repo, teams),
 		repository_05: false,
 		repository_06: false,
 		repository_07: false,
