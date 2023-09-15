3eb0c46d512db6faef182f7e2c0ad4f8d49c12c6
Julia Branke <Julia.Branke@guardian.co.uk>
2023-09-15 14:50:16 +0100

    Delete content of table repocop_github_repository_rules before adding mandatory timestamp
    Co-authored-by: Akash <akash1810@users.noreply.github.com>

--- packages/repocop/prisma/migrations/20230915132202_add_field_evaluated_on_to_repocop_github_repository_rules/migration.sql
+++ packages/repocop/prisma/migrations/20230915132202_add_field_evaluated_on_to_repocop_github_repository_rules/migration.sql
@@ -4,5 +4,7 @@
   - Added the required column `evaluated_on` to the `repocop_github_repository_rules` table without a default value. This is not possible if the table is not empty.
 
 */
+-- DeleteEntries
+DELETE FROM repocop_github_repository_rules;
 -- AlterTable
 ALTER TABLE "repocop_github_repository_rules" ADD COLUMN     "evaluated_on" TIMESTAMP(3) NOT NULL;
