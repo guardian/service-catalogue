77747ed822aa7b60be54a8c6a4dda10af9c0fd28
Natasha <67543397+NovemberTang@users.noreply.github.com>
2023-09-28 16:06:16 +0100

    double check whether branch is already protected

--- packages/branch-protector/src/index.ts
+++ packages/branch-protector/src/index.ts
@@ -1,11 +1,39 @@
 import { Octokit } from 'octokit';
 
-const authToken = process.env.GITHUB_ACCESS_TOKEN;
+//TODO: move to a common place
+function getEnvOrThrow(key: string): string {
+	const value = process.env[key];
+	if (value === undefined) {
+		throw new Error(`Environment variable ${key} is not set`);
+	}
+	return value;
+}
+
+const authToken: string = getEnvOrThrow('GITHUB_ACCESS_TOKEN');
+
+async function isMainBranchProtected(
+	octokit: Octokit,
+	fullRepoName: string,
+): Promise<boolean> {
+	const owner = fullRepoName.split('/')[0]!;
+	const repo = fullRepoName.split('/')[1]!;
+	const data = await octokit.rest.repos.get({ owner: owner, repo: repo });
+	const branch = data.data.default_branch;
+	const branchData = await octokit.rest.repos.getBranch({
+		owner,
+		repo,
+		branch,
+	});
+	return branchData.data.protected;
+}
 
 export async function main() {
-	const octokit = new Octokit({ auth: authToken });
+	const octokit: Octokit = new Octokit({ auth: authToken });
 
-	const data = await octokit.auth();
-
-	console.log(data);
+	const exampleRepo = 'guardian/service-catalogue';
+	const isProtected: boolean = await isMainBranchProtected(
+		octokit,
+		exampleRepo,
+	);
+	console.log(`Is ${exampleRepo} protected? ${isProtected.toString()}`);
 }
