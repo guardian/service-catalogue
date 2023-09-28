8a710b875f12ab2afafc189812647295dec94f98
akash1810 <akash1810@users.noreply.github.com>
2023-09-28 10:02:20 +0100

    fix(prisma): Bring `view_repo_ownership` view into Prisma
    Move the definition of this table into Prisma so that it can be applied during migrations,
    and used in the client.

---
+++ packages/repocop/prisma/migrations/20230928084104_view_repo_ownership/migration.sql
@@ -0,0 +1,13 @@
+create or replace view view_repo_ownership as
+select ght.id        as "github_team_id"
+     , ght.name      as "github_team_name"
+     , tr.full_name  as "repo_name" --deprecated. use full_name below for consistency with other tables
+     , tr.full_name  as "full_name"
+     , tr.role_name
+     , tr.archived
+     , gtt.team_name as "galaxies_team"
+     , gtt.team_contact_email
+from github_team_repositories tr
+         join github_teams ght on tr.team_id = ght.id
+         left join galaxies_teams_table gtt on ght.slug = gtt.team_primary_github_team
+where tr.role_name = 'admin';
