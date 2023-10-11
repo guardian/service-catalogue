58281641de1da4091800e978f5feb3eb4b6d5c55
TJ Silver <thalia.silver@guardian.co.uk>
2023-10-11 15:48:35 +0100

    fix: separate local setup migration from new table creation

--- packages/repocop/script/migrate.sh
+++ packages/repocop/script/migrate.sh
@@ -6,14 +6,19 @@ red='\x1B[0;31m'
 bold='\x1B[1m'
 plain='\x1B[0m'
 
-migrateDEV() {
+devSetup() {
+  echo -e "${bold}Setting up local DATABASE_URL and resetting migrations${plain}"
   source ../../.env
-
   export DATABASE_URL="postgresql://$LOCAL_DB_USER:$LOCAL_DB_PASSWORD@$LOCAL_DB_HOSTNAME:$LOCAL_DB_PORT/postgres"
-
   # clear existing local migrations table (if applicable)
   # and apply all migrations
   npx prisma migrate reset --force --schema "prisma/schema.prisma"
+}
+
+migrateDEV() {
+  migration_name=$1
+  echo -e "${bold}Creating migration $migration_name${plain}"
+  npx prisma migrate dev --name "$migration_name"
 }
 
 getDbUrl() {
@@ -21,12 +26,12 @@ getDbUrl() {
   allSecrets=$(aws secretsmanager list-secrets --profile deployTools --region eu-west-1 | jq '.SecretList')
   secret_id=$(jq -r --arg stage "$stage" '.[] | select(.Tags[] as $t | any($t.Key=="Stack" and $t.Value=="deploy")) | select(.Tags[] as $t | any($t.Key=="Stage" and $t.Value==$stage)) | select(.Tags[] as $t | any($t.Key=="App" and $t.Value=="service-catalogue")) | select(.Name | test("PostgresInstance")) | .Name' <<< "$allSecrets")
   dbSecretJson=$(aws secretsmanager get-secret-value --secret-id "$secret_id" --profile deployTools --region eu-west-1 | jq -c '[.SecretString | fromjson]')
-  db_username=$(jq -r '.[] | .username' <<< "$dbSecretJson")
+  db_usermigration_name=$(jq -r '.[] | .usermigration_name' <<< "$dbSecretJson")
   db_password=$(jq -r '.[] | .password' <<< "$dbSecretJson")
   db_host=$(jq -r '.[] | .host' <<< "$dbSecretJson")
   db_port=$(jq -r '.[] | .port' <<< "$dbSecretJson")
 
-  export DATABASE_URL="postgresql://$db_username:$db_password@$db_host:$db_port/postgres"
+  export DATABASE_URL="postgresql://$db_usermigration_name:$db_password@$db_host:$db_port/postgres"
 }
 
 note="NOTE: You need to be on the VPN to migrate. \nMigration may fail if the table(s) already exist in the database.\nSee https://www.prisma.io/docs/guides/migrate/production-troubleshooting#failed-migration for how to fix."
@@ -57,12 +62,19 @@ migratePROD() {
   continueQuery "PROD"
 }
 
-# Read in input flag
+# Read in input stage(s)
 stage=$1
+migration_name=$2
 
 if [ "$stage" == "--dev" ]
 then
-  migrateDEV
+  devSetup
+  if [ -n "$migration_name" ]
+  then
+    migrateDEV "$migration_name"
+  else
+    :
+  fi
 elif [ "$stage" == "--code" ]
 then
   migrateCODE
@@ -70,5 +82,5 @@ elif [ "$stage" == "--prod" ]
 then
   migratePROD
 else
-  echo -e "Stage '$stage' not recognised, please try again with either '--dev', '--code' or '--prod' flags"
+  echo -e "Stage '$stage' not recognised, please try again."
 fi
