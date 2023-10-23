56b884cebadd7bcdbdd436b12a7face7f7e85a08
Julia Branke <Julia.Branke@guardian.co.uk>
2023-10-23 16:49:10 +0100

    Add comment, remove console.log
    Co-authored-by: Akash <akash1810@users.noreply.github.com>
    Co-authored-by: Thalia <tjsilver@users.noreply.github.com>

--- packages/repocop/src/run-locally.ts
+++ packages/repocop/src/run-locally.ts
@@ -1,7 +1,7 @@
 import { config } from 'dotenv';
 import { main } from './index';
 
-// eslint-disable-next-line @typescript-eslint/restrict-template-expressions -- wip
+// eslint-disable-next-line @typescript-eslint/restrict-template-expressions -- on a dev machine we can be pretty sure there is a home directory
 config({ path: `${process.env.HOME}/.gu/service_catalogue/.env.local` });
 
 if (require.main === module) {
@@ -12,8 +12,6 @@ if (require.main === module) {
 
 	// Set this to 'false' to disable SQL query logging
 	process.env.QUERY_LOGGING = 'true';
-
-	console.log(process.env.ANGHAMMARAD_SNS_ARN);
 
 	void (async () => await main())();
 }
