d2d2d8961b2e237ba78fa7949f43130a614689ab
akash1810 <akash1810@users.noreply.github.com>
2023-10-24 11:00:36 +0100

    refactor(repocop): Read straight from `.env` file to reduce duplication

--- packages/repocop/src/run-locally.ts
+++ packages/repocop/src/run-locally.ts
@@ -5,13 +5,5 @@ import { main } from './index';
 config({ path: `${process.env.HOME}/.gu/service_catalogue/.env.local` });
 
 if (require.main === module) {
-	process.env.STAGE = process.env.LOCAL_STAGE;
-	process.env.DATABASE_HOSTNAME = process.env.LOCAL_DB_HOSTNAME;
-	process.env.DATABASE_PASSWORD = process.env.LOCAL_DB_PASSWORD;
-	process.env.DATABASE_USER = process.env.LOCAL_DB_USER;
-
-	// Set this to 'false' to disable SQL query logging
-	process.env.QUERY_LOGGING = 'true';
-
 	void (async () => await main())();
 }
