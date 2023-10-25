8ab335034daf57197e9dd5f11e32fc0f0367e44f
TJ Silver <thalia.silver@guardian.co.uk>
2023-10-25 11:13:18 +0100

    refactor: append non-token env vars and tidy up script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -71,13 +71,12 @@ check_credentials() {
 
 setup_environment() {
   step "Setting up Service Catalogue environment"
-  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-  LOCAL_ENV_FILE_DIR=$HOME/.gu/service_catalogue
+  local_env_file_dir=$HOME/.gu/service_catalogue
 
-  LOCAL_ENV_FILE=$LOCAL_ENV_FILE_DIR/.env.local
+  local_env_file=$local_env_file_dir/.env.local
 
-  SIZE_THRESHOLD=1
+  size_threshold=1
 
   GALAXIES_BUCKET=$(aws ssm get-parameter --name /INFRA/deploy/services-api/galaxies-bucket-name --profile deployTools --region eu-west-1 | jq '.Parameter.Value' | tr -d '"')
 
@@ -85,61 +84,64 @@ setup_environment() {
 
   QUEUE_URL=$(aws sqs get-queue-url --queue-name branch-protector-queue-CODE.fifo --profile deployTools --region eu-west-1 | jq '.QueueUrl' | tr -d '"')
 
-  TOKEN_TEXT="# See https://github.com/settings/tokens?type=beta
+  github_info_url="https://github.com/settings/tokens?type=beta"
+
+  snyk_info_url="https://docs.snyk.io/snyk-api-info/authentication-for-api"
+
+  token_text="# See $github_info_url
 GITHUB_ACCESS_TOKEN=
 
-# See https://docs.snyk.io/snyk-api-info/authentication-for-api
-SNYK_TOKEN=
-
-"
-  ENV_VAR_TEXT="
+# See $snyk_info_url
+SNYK_TOKEN="
+  
+  env_var_text="
 GALAXIES_BUCKET=${GALAXIES_BUCKET}
 ANGHAMMARAD_SNS_ARN=${ANGHAMMARAD_SNS_ARN}
 QUEUE_URL=${QUEUE_URL}
 "
 
   # Check if .env.local file exists in ~/.gu/service_catalogue/
-  echo "Checking for .env.local file in $LOCAL_ENV_FILE_DIR"
-  if [ -e "$LOCAL_ENV_FILE" ]
+  echo "Checking for .env.local file in $local_env_file_dir"
+  if [ -e "$local_env_file" ]
   then
-    echo "Found .env.local file in $LOCAL_ENV_FILE_DIR"
+    echo "Found .env.local file in $local_env_file_dir"
     # Get file size in bytes
-    FILE_SIZE=$(wc -c < "$LOCAL_ENV_FILE")
+    file_size=$(wc -c < "$local_env_file")
 
     # Check if file is empty - don't want to overwrite any existing tokens
-    if [ "$FILE_SIZE" -gt "$SIZE_THRESHOLD" ]
+    if [ "$file_size" -gt "$size_threshold" ]
     then
       echo "File is not empty. Appending required environment variables only"
-      echo "$ENV_VAR_TEXT" >> "$LOCAL_ENV_FILE"
+      echo "$env_var_text" >> "$local_env_file"
     else
-      echo "File is empty, adding token names$"
-      echo "$TOKEN_TEXT" >> "$LOCAL_ENV_FILE"
+      echo "File is empty, adding Github and Snyk token names"
+      echo "$token_text" >> "$local_env_file"
       echo "Appending required environment variables"
-      echo "$ENV_VAR_TEXT" >> "$LOCAL_ENV_FILE"
+      echo "$env_var_text" >> "$local_env_file"
     fi
   else
-    echo "No .env.local file found - creating it in $LOCAL_ENV_FILE_DIR"
+    echo "No .env.local file found - creating it in $local_env_file_dir"
     mkdir -p "$HOME"/.gu/service_catalogue
-    touch -a "$LOCAL_ENV_FILE_DIR"/.env.local
-    echo "Adding token names and required environment variables"
-    echo "$TOKEN_TEXT" >> "$LOCAL_ENV_FILE"
-    echo "$ENV_VAR_TEXT" >> "$LOCAL_ENV_FILE"
+    touch -a "$local_env_file_dir"/.env.local
+    echo "Adding Github and Snyk token names and required environment variables"
+    echo "$token_text" >> "$local_env_file"
+    echo "$env_var_text" >> "$local_env_file"
   fi
 
-source "$LOCAL_ENV_FILE"
+source "$local_env_file"
 
   # Check if GitHub token is set
   if [ -z "$GITHUB_ACCESS_TOKEN" ]
   then
     echo -e "${yellow}Please create or retrieve a GitHub token${clear}.
-Visit ${cyan}https://github.com/settings/tokens?type=beta${clear}, and add it to ${cyan}$LOCAL_ENV_FILE${clear}"
+Visit ${cyan}$github_info_url${clear}, and add it to ${cyan}$local_env_file${clear}"
   fi
 
   # Check if Snyk token is set
   if [ -z "$SNYK_TOKEN" ]
   then
     echo -e "${yellow}Please create or retrieve a Snyk token${clear}.
-Visit ${cyan}https://docs.snyk.io/snyk-api-info/authentication-for-api${clear}, and add it to ${cyan}$LOCAL_ENV_FILE${clear}"
+Visit ${cyan}$snyk_info_url${clear}, and add it to ${cyan}$local_env_file${clear}"
   fi
 }
 
