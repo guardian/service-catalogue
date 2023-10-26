6c96844a098e6efbf88a2500414ae5ea70171239
Julia Branke <Julia.Branke@guardian.co.uk>
2023-10-26 17:55:21 +0100

    Migration for cp_last_synced view

--- sql/aws_accounts_view.sql
+++ sql/aws_accounts_view.sql
@@ -3,13 +3,13 @@
  see prisma migration 20231026134216_view_aws_accounts
  in directory packages/repocop/prisma/migrations
  */
-CREATE OR REPLACE VIEW aws_accounts AS
-SELECT DISTINCT acc.id,
-                acc.name,
-                acc.email,
-                acc.status,
-                acc.joined_timestamp,
-                COALESCE(ou.name, 'ROOT') AS "organizational_unit"
-FROM aws_organizations_accounts acc
-         LEFT JOIN aws_organizations_account_parents par  ON acc.id = par.id
-         LEFT JOIN aws_organizations_organizational_units ou on par.parent_id = ou.id;
+-- CREATE OR REPLACE VIEW aws_accounts AS
+-- SELECT DISTINCT acc.id,
+--                 acc.name,
+--                 acc.email,
+--                 acc.status,
+--                 acc.joined_timestamp,
+--                 COALESCE(ou.name, 'ROOT') AS "organizational_unit"
+-- FROM aws_organizations_accounts acc
+--          LEFT JOIN aws_organizations_account_parents par  ON acc.id = par.id
+--          LEFT JOIN aws_organizations_organizational_units ou on par.parent_id = ou.id;
--- sql/cq_last_synced_view.sql
+++ sql/cq_last_synced_view.sql
@@ -1,35 +1,37 @@
 /*
  The view cq_last_synced
  Shows the last time a table was synced
+ see prisma migration 20231026164314_cp_last_synced
+ in directory packages/repocop/prisma/migrations
  */
-DROP VIEW IF EXISTS cq_last_synced;
-
-DO $$
-    DECLARE
-        tbl TEXT;
-        strSQL TEXT = '';
-    BEGIN
-        -- iterate over every table in our information_schema that has an `arn` column available
-        FOR tbl IN
-            SELECT DISTINCT table_name
-            FROM information_schema.columns
-            WHERE COLUMN_NAME IN ('_cq_sync_time')
-            LOOP
-                -- UNION each table query to create one view
-                IF NOT (strSQL = ''::TEXT) THEN
-                    strSQL = strSQL || ' UNION ALL ';
-                END IF;
-                -- create an SQL query to select from table and transform it into our resources view schema
-                strSQL = strSQL || FORMAT(E'
-        (SELECT _cq_sync_time, %L AS table_name
-        FROM %s limit 1) ', tbl, tbl);
-            END LOOP;
-
-        IF strSQL = ''::TEXT THEN
-            RAISE EXCEPTION 'No tables found.';
-        ELSE
-            RAISE NOTICE '%', strSQL;
-            EXECUTE FORMAT('CREATE VIEW cq_last_synced AS (%s)', strSQL);
-        END IF;
-
-    END $$;
+-- DROP VIEW IF EXISTS cq_last_synced;
+--
+-- DO $$
+--     DECLARE
+--         tbl TEXT;
+--         strSQL TEXT = '';
+--     BEGIN
+--         -- iterate over every table in our information_schema that has an `arn` column available
+--         FOR tbl IN
+--             SELECT DISTINCT table_name
+--             FROM information_schema.columns
+--             WHERE COLUMN_NAME IN ('_cq_sync_time')
+--             LOOP
+--                 -- UNION each table query to create one view
+--                 IF NOT (strSQL = ''::TEXT) THEN
+--                     strSQL = strSQL || ' UNION ALL ';
+--                 END IF;
+--                 -- create an SQL query to select from table and transform it into our resources view schema
+--                 strSQL = strSQL || FORMAT(E'
+--         (SELECT _cq_sync_time, %L AS table_name
+--         FROM %s limit 1) ', tbl, tbl);
+--             END LOOP;
+--
+--         IF strSQL = ''::TEXT THEN
+--             RAISE EXCEPTION 'No tables found.';
+--         ELSE
+--             RAISE NOTICE '%', strSQL;
+--             EXECUTE FORMAT('CREATE VIEW cq_last_synced AS (%s)', strSQL);
+--         END IF;
+--
+--     END $$;
