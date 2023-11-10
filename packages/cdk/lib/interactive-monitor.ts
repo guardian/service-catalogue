fa3b6f7bc7102292559f47934c87342198f1d36f
Natasha <67543397+NovemberTang@users.noreply.github.com>
2023-11-10 19:11:35 +0000

    provide app name

--- packages/cdk/lib/interactive-monitor.ts
+++ packages/cdk/lib/interactive-monitor.ts
@@ -4,20 +4,21 @@ import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Topic } from 'aws-cdk-lib/aws-sns';
 import { LambdaSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
 
+const app = 'interactive-monitor';
+
 export class InteractiveMonitor {
 	public topic: Topic;
 	constructor(guStack: GuStack) {
 		const topic = new Topic(guStack, 'Topic', {
-			topicName: 'interactive-monitor-topic',
+			topicName: `${app}-topic`,
 		});
 
-		const lambda = new GuLambdaFunction(guStack, 'interactive-monitor', {
-			app: guStack.app ?? '',
-			fileName: 'interactive-monitor.zip',
+		const lambda = new GuLambdaFunction(guStack, app, {
+			app,
+			fileName: `${app}.zip`,
 			handler: 'index.handler',
 			runtime: Runtime.NODEJS_18_X,
 			environment: {
-				TOPIC_ARN: topic.topicArn,
 				BUCKET: '???',
 				GITHUB_CREDENTIALS: '???',
 			},
