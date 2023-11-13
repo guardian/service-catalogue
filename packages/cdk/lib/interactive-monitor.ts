5fa80ec70479fb8ad5e96fdbc17c97ce895510cb
Natasha <67543397+NovemberTang@users.noreply.github.com>
2023-11-13 08:52:55 +0000

    add github app to CDK, validate via lambda

--- packages/cdk/lib/interactive-monitor.ts
+++ packages/cdk/lib/interactive-monitor.ts
@@ -1,26 +1,32 @@
 import { type GuStack } from '@guardian/cdk/lib/constructs/core';
 import { GuLambdaFunction } from '@guardian/cdk/lib/constructs/lambda';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 import { Topic } from 'aws-cdk-lib/aws-sns';
 import { LambdaSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
 
-const app = 'interactive-monitor';
-
+const service = 'interactive-monitor';
 export class InteractiveMonitor {
 	public topic: Topic;
 	constructor(guStack: GuStack) {
+		const app = guStack.app ?? 'service-catalogue'; //shouldn't be undefined, but make linter happy
+
 		const topic = new Topic(guStack, 'Topic', {
-			topicName: `${app}-topic`,
+			topicName: `${service}-topic`,
 		});
 
-		const lambda = new GuLambdaFunction(guStack, app, {
-			app,
-			fileName: `${app}.zip`,
+		const githubCredentials = new Secret(guStack, `${service}-github-app`, {
+			secretName: `/${guStack.stack}/${guStack.stage}/${app}/${service}-github-app`,
+		});
+
+		const lambda = new GuLambdaFunction(guStack, service, {
+			app: service,
+			fileName: `${service}.zip`,
 			handler: 'index.handler',
 			runtime: Runtime.NODEJS_18_X,
 			environment: {
 				BUCKET: '???',
-				GITHUB_CREDENTIALS: '???',
+				GITHUB_APP_SECRET: githubCredentials.secretName,
 			},
 			reservedConcurrentExecutions: 1,
 		});
--- packages/interactive-monitor/src/index.ts
+++ packages/interactive-monitor/src/index.ts
@@ -1,3 +1,8 @@
-export function handler() {
+import { getGitHubAppConfig } from 'common/functions';
+import type { GitHubAppConfig } from 'common/types';
+
+export async function handler() {
+	const githubAppConfig: GitHubAppConfig = await getGitHubAppConfig();
+	githubAppConfig.strategyOptions.appId;
 	console.log('Hello, world!');
 }
