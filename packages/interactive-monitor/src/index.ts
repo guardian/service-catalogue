719f9d7cafcf77a493788af06f5050be343a0435
Natasha <67543397+NovemberTang@users.noreply.github.com>
2023-11-13 15:17:10 +0000

    find out whether a repo has been generated from an interactive template

--- packages/interactive-monitor/src/index.ts
+++ packages/interactive-monitor/src/index.ts
@@ -1,8 +1,33 @@
-import { getGitHubAppConfig } from 'common/functions';
+import { getGitHubAppConfig, getGithubClient } from 'common/functions';
 import type { GitHubAppConfig } from 'common/types';
+import type { Octokit } from 'octokit';
+
+async function isFromInteractiveTemplate(
+	repo: string,
+	octokit: Octokit,
+): Promise<boolean> {
+	const repoData = await octokit.rest.repos.get({
+		owner: 'guardian',
+		repo,
+	});
+
+	return (
+		repoData.data.template_repository?.name.includes(
+			'interactive-atom-template',
+		) != null
+	);
+}
 
 export async function handler() {
 	const githubAppConfig: GitHubAppConfig = await getGitHubAppConfig();
 	githubAppConfig.strategyOptions.appId;
-	console.log('Hello, world!');
+
+	const octokit: Octokit = await getGithubClient(githubAppConfig);
+
+	console.log(
+		await isFromInteractiveTemplate(
+			'interactive-cabinet-reshuffle-2022',
+			octokit,
+		),
+	);
 }
