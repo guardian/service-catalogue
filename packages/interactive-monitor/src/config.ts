e8b4e214ce338ac84d43e8511843f455c23d1485
Natasha <67543397+NovemberTang@users.noreply.github.com>
2023-11-15 17:15:09 +0000

    apply interactive topic if necessary (#478)
    * apply topic if necessary
    
    * make stage aware, remove log lines
    
    * address pr comments

---
+++ packages/interactive-monitor/src/config.ts
@@ -0,0 +1,12 @@
+interface Config {
+	/**
+	 * The stage to run in.
+	 */
+	stage: string;
+}
+
+export function getConfig(): Config {
+	return {
+		stage: process.env['STAGE'] ?? 'DEV',
+	};
+}
--- packages/interactive-monitor/src/index.ts
+++ packages/interactive-monitor/src/index.ts
@@ -2,35 +2,47 @@ import type { SNSHandler } from 'aws-lambda';
 import { getGitHubAppConfig, getGithubClient } from 'common/functions';
 import type { GitHubAppConfig } from 'common/types';
 import type { Octokit } from 'octokit';
+import { getConfig } from './config';
 
 async function isFromInteractiveTemplate(
 	repo: string,
 	octokit: Octokit,
 ): Promise<boolean> {
+	console.log('retrieving repo data');
 	const repoData = await octokit.rest.repos.get({
 		owner: 'guardian',
 		repo,
 	});
 
 	const prefix = 'interactive-atom-template';
-
+	console.log('checking if repo is from interactive template');
 	return repoData.data.template_repository?.name.includes(prefix) ?? false;
 }
 
 export const handler: SNSHandler = async (event) => {
+	const config = getConfig();
 	if (event.Records.length !== 1) {
 		throw new Error(
 			`Expected exactly one record, but got ${event.Records.length}`,
 		);
 	} else {
-		const message = event.Records[0]!.Sns.Message;
-		console.log('received message', message);
-
+		const repo = event.Records[0]!.Sns.Message;
+		const owner = 'guardian';
+		console.log('received repo', repo);
 		const githubAppConfig: GitHubAppConfig = await getGitHubAppConfig();
-		githubAppConfig.strategyOptions.appId;
-
 		const octokit: Octokit = await getGithubClient(githubAppConfig);
 
-		console.log(await isFromInteractiveTemplate(message, octokit));
+		const isInteractive = await isFromInteractiveTemplate(repo, octokit);
+		const topics = (await octokit.rest.repos.getAllTopics({ owner, repo })).data
+			.names;
+
+		if (isInteractive && config.stage === 'PROD') {
+			console.log(`${repo}is from interactive template`);
+			const names = topics.concat(['interactive']);
+			await octokit.rest.repos.replaceAllTopics({ owner, repo, names });
+			console.log(`added interactive topic to ${repo}`);
+		} else {
+			console.log('No action taken');
+		}
 	}
 };
