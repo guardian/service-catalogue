6264d4807aa8323d112504891613b6700fabcbb7
akash1810 <akash1810@users.noreply.github.com>
2023-11-22 14:48:44 +0000

    fix(data-audit): Add the prisma client to the lambda artifact

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -4,21 +4,24 @@ set -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ROOT_DIR=$(realpath "${DIR}/..")
 
-createRepocopZip() {
-  echo "Creating repocop package"
+createLambdaWithPrisma() {
+  name=$1
+
+  echo "Creating $name package"
+
   # Copy the Prisma schema file to the dist directory
-  cp -r "$ROOT_DIR/packages/common/prisma" "$ROOT_DIR/packages/repocop/dist"
+  cp -r "$ROOT_DIR/packages/common/prisma" "$ROOT_DIR/packages/$name/dist"
 
   # Copy the generated Prisma client
-  mkdir -p "$ROOT_DIR/packages/repocop/dist/node_modules"
-  cp -r "$ROOT_DIR/node_modules/@prisma" "$ROOT_DIR/packages/repocop/dist/node_modules/@prisma"
-  cp -r "$ROOT_DIR/node_modules/prisma" "$ROOT_DIR/packages/repocop/dist/node_modules/prisma"
-  cp -r "$ROOT_DIR/node_modules/.prisma" "$ROOT_DIR/packages/repocop/dist/node_modules/.prisma"
+  mkdir -p "$ROOT_DIR/packages/$name/dist/node_modules"
+  cp -r "$ROOT_DIR/node_modules/@prisma" "$ROOT_DIR/packages/$name/dist/node_modules/@prisma"
+  cp -r "$ROOT_DIR/node_modules/prisma" "$ROOT_DIR/packages/$name/dist/node_modules/prisma"
+  cp -r "$ROOT_DIR/node_modules/.prisma" "$ROOT_DIR/packages/$name/dist/node_modules/.prisma"
 
   # Create a zip file of the dist directory
   (
-    cd "$ROOT_DIR/packages/repocop/dist"
-    zip -qr repocop.zip .
+    cd "$ROOT_DIR/packages/$name/dist"
+    zip -qr $name.zip .
   )
 }
 
@@ -34,4 +37,8 @@ npm ci
 npm run typecheck & npm run lint
 npm run test
 npm run synth & npm run build
-createZip "branch-protector" & createZip "interactive-monitor" & createRepocopZip
+
+createZip "branch-protector" & \
+  createZip "interactive-monitor" & \
+  createLambdaWithPrisma "repocop" & \
+  createLambdaWithPrisma "data-audit"
