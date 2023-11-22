e132f10fb313ecd89daff2aaad1d797f6e1f4a0d
akash1810 <akash1810@users.noreply.github.com>
2023-11-22 18:24:32 +0000

    feat(data-audit): Check the number of accounts we have data for
    Check that the number of accounts in the database, is the same number of accounts in the AWS Organisation.

--- packages/data-audit/src/index.ts
+++ packages/data-audit/src/index.ts
@@ -1,10 +1,14 @@
+import {
+	OrganizationsClient,
+	paginateListAccounts,
+} from '@aws-sdk/client-organizations';
 import { PrismaClient } from '@prisma/client';
+import { awsClientConfig } from 'common/aws';
+import type { Config } from './config';
 import { getConfig } from './config';
 
-export async function main() {
-	const config = await getConfig();
-
-	const prisma = new PrismaClient({
+function numberOfAwsAccountsFromDatabase(config: Config): Promise<number> {
+	const client = new PrismaClient({
 		datasources: {
 			db: {
 				url: config.databaseConnectionString,
@@ -19,7 +23,33 @@ export async function main() {
 			],
 		}),
 	});
+	return client.aws_accounts.count();
+}
 
-	const awsAccounts = await prisma.aws_accounts.count();
-	console.log(`There are ${awsAccounts} AWS accounts in the database`);
+async function numberOfAwsAccountsFromAws(config: Config): Promise<number> {
+	const client = new OrganizationsClient(awsClientConfig(config.stage));
+
+	let total = 0;
+	for await (const page of paginateListAccounts(
+		{
+			client,
+			pageSize: 10,
+		},
+		{},
+	)) {
+		total += page.Accounts?.length ?? 0;
+	}
+	return total;
+}
+
+export async function main() {
+	const config = await getConfig();
+
+	const totalFromDb = await numberOfAwsAccountsFromDatabase(config);
+	const totalFromAws = await numberOfAwsAccountsFromAws(config);
+
+	const status = totalFromDb === totalFromAws ? 'PASS' : 'FAIL';
+	console.log(
+		`${status} AWS accounts check. DB: ${totalFromDb} AWS: ${totalFromAws}`,
+	);
 }
