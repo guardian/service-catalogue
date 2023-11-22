c92539184c8d37934ec1620c761324f5a2e5169c
akash1810 <akash1810@users.noreply.github.com>
2023-11-22 08:13:04 +0000

    docs: Add SQL used to manually create database user

--- sql/dbuser.sql
+++ sql/dbuser.sql
@@ -8,3 +8,8 @@ GRANT rds_iam TO repocop;
 
 -- This table is created via a Prisma migration
 GRANT ALL ON public.repocop_github_repository_rules TO repocop;
+
+CREATE USER dataaudit WITH LOGIN;
+GRANT USAGE ON SCHEMA public to dataaudit;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO dataaudit;
+GRANT rds_iam TO dataaudit;
