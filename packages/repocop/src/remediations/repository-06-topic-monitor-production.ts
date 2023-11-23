0c3a86c12e9864aa045f4deb7ddf18eed0e11491
TJ Silver <thalia.silver@guardian.co.uk>
2023-11-23 12:16:16 +0000

    refactor: improve typing on functions

--- packages/repocop/src/remediations/repository-06-topic-monitor-production.ts
+++ packages/repocop/src/remediations/repository-06-topic-monitor-production.ts
@@ -6,14 +6,15 @@ export async function findReposInProdWithoutProductionTopic(
 	unarchivedRepos: github_repositories[],
 ) {
 	console.log('Discovering Cloudformation stacks with PROD or INFRA tags');
-	const reposWithoutProductionTopic = unarchivedRepos
+	const reposWithoutProductionTopic: string[] = unarchivedRepos
 		.filter(
 			(repo) =>
 				!repo.topics.includes('production') &&
 				!repo.topics.includes('interactive'),
 		)
 		.map((repo) => repo.full_name)
-		.filter((name) => !name?.includes('interactive'));
+		.filter((name) => !name?.includes('interactive'))
+		.filter((name) => !!name) as string[];
 	console.log(
 		`Found ${reposWithoutProductionTopic.length} repositories without a production topic`,
 	);
@@ -28,7 +29,7 @@ export async function findReposInProdWithoutProductionTopic(
 		.filter((r) => !!r) as string[];
 
 	const reposInProdWithoutProductionTopic = reposWithoutProductionTopic.filter(
-		(repoName) => repoName && guReposWithProdCfnStacks.includes(repoName),
+		(repoName) => guReposWithProdCfnStacks.includes(repoName),
 	);
 
 	console.log(
