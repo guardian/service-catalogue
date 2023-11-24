05d26609df8fc0c031552353fdb2dd65ae2cbc73
Natasha <67543397+NovemberTang@users.noreply.github.com>
2023-11-24 09:31:13 +0000

    search for config.json and try to extract path

--- packages/interactive-monitor/src/index.ts
+++ packages/interactive-monitor/src/index.ts
@@ -1,3 +1,4 @@
+import type { RestEndpointMethodTypes } from '@octokit/plugin-rest-endpoint-methods';
 import type { SNSHandler } from 'aws-lambda';
 import { stageAwareOctokit } from 'common/functions';
 import type { Octokit } from 'octokit';
@@ -16,6 +17,44 @@ async function isFromInteractiveTemplate(
 	return repoData.data.template_repository?.name.includes(prefix) ?? false;
 }
 
+type ContentResponse =
+	RestEndpointMethodTypes['repos']['getContent']['response'];
+
+interface FileMetadata {
+	name: string;
+	content: string;
+}
+
+interface ConfigJsonFile {
+	path: string;
+}
+
+function decodeFile(response: ContentResponse): ConfigJsonFile {
+	const deserialisedResponse = response.data as FileMetadata;
+	const fileContents = JSON.parse(
+		atob(deserialisedResponse.content),
+	) as ConfigJsonFile;
+
+	return fileContents;
+}
+async function getConfigJsonFromGithub(
+	octokit: Octokit,
+	repo: string,
+	owner: string,
+): Promise<ConfigJsonFile | undefined> {
+	try {
+		const configFile: ContentResponse = await octokit.rest.repos.getContent({
+			owner,
+			repo,
+			path: 'config.json',
+		});
+		return decodeFile(configFile);
+	} catch (e) {
+		console.log(`No config.json found for ${repo}`);
+		return undefined;
+	}
+}
+
 async function applyTopics(repo: string, owner: string, octokit: Octokit) {
 	console.log(`Applying interactive topic to ${repo}`);
 	const topics = (await octokit.rest.repos.getAllTopics({ owner, repo })).data
@@ -27,10 +66,16 @@ async function applyTopics(repo: string, owner: string, octokit: Octokit) {
 export async function assessRepo(repo: string, owner: string, stage: string) {
 	const octokit = await stageAwareOctokit(stage);
 
+	const file = await getConfigJsonFromGithub(octokit, repo, owner);
+	const configJsonExists = file !== undefined;
+
 	const isInteractive = await isFromInteractiveTemplate(repo, owner, octokit);
 	const onProd = stage === 'PROD';
 	if (isInteractive && onProd) {
 		await applyTopics(repo, owner, octokit);
+	} else if (configJsonExists && onProd) {
+		console.log(`Found potential s3 path for ${repo}`);
+		console.log('TODO: search s3 for path');
 	} else {
 		const reason =
 			(!isInteractive ? ' Repo not from interactive template.' : '') +
--- packages/interactive-monitor/src/run-locally.ts
+++ packages/interactive-monitor/src/run-locally.ts
@@ -3,5 +3,5 @@ import { config } from 'dotenv';
 import { assessRepo } from './index';
 
 config({ path: `${homedir()}/.gu/service_catalogue/.env.local` });
-const testRepo = 'service-catalogue';
+const testRepo = 'interactive-atom-template-2019'; //'service-catalogue';
 void (async () => await assessRepo(testRepo, 'guardian', 'DEV'))();
