78aca7d3c5c28e0371e0b9c516da1b034518fe43
akash1810 <akash1810@users.noreply.github.com>
2023-11-30 15:11:04 +0000

    chore: Migrate to `tsx` in the `interactive-monitor` package

--- packages/interactive-monitor/src/run-locally.ts
+++ packages/interactive-monitor/src/run-locally.ts
@@ -3,7 +3,9 @@ import { config } from 'dotenv';
 import { getConfig } from './config';
 import { assessRepo } from './index';
 
+config({ path: `../../.env` }); // Load `.env` file at the root of the repository
 config({ path: `${homedir()}/.gu/service_catalogue/.env.local` });
+
 const testRepo = 'ofm-awards-label-2019-atom';
 const devConfig = getConfig();
 void (async () => await assessRepo(testRepo, 'guardian', devConfig))();
