38526a20db76957f157588d09fd311a840f6c77c
Akash Askoolum <akash1810@users.noreply.github.com>
2023-12-04 10:33:59 +0000

    feat(cdk): Add link to logs within RepoCop alarms (#543)
    * feat: Generate a deep-link to Central ELK
    
    * refactor(cli): Use shared function to get Central ELK link
    
    * feat(cdk): Add link to logs within RepoCop alarms

---
+++ packages/common/src/logs.test.ts
@@ -0,0 +1,30 @@
+import { getCentralElkLink } from './logs';
+
+describe('getCentralElkUrl', () => {
+	it('should form a valid link', () => {
+		const actual = getCentralElkLink({
+			filters: {
+				stack: 'deploy',
+				stage: 'CODE',
+				app: 'service-catalogue',
+			},
+		});
+		const expected =
+			"https://logs.gutools.co.uk/s/devx/app/discover#/?_g=(filters:!((query:(match_phrase:(stack:'deploy'))),(query:(match_phrase:(stage:'CODE'))),(query:(match_phrase:(app:'service-catalogue')))))";
+		expect(actual).toEqual(expected);
+	});
+
+	it('should form a valid link with columns', () => {
+		const actual = getCentralElkLink({
+			filters: {
+				stack: 'deploy',
+				stage: 'CODE',
+				app: 'service-catalogue',
+			},
+			columns: ['message'],
+		});
+		const expected =
+			"https://logs.gutools.co.uk/s/devx/app/discover#/?_g=(filters:!((query:(match_phrase:(stack:'deploy'))),(query:(match_phrase:(stage:'CODE'))),(query:(match_phrase:(app:'service-catalogue')))))&_a=(columns:!(message))";
+		expect(actual).toEqual(expected);
+	});
+});
