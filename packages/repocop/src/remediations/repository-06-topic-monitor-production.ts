ad56c314dc3af2b5e2030382cca993d09071d2d7
Natasha <67543397+NovemberTang@users.noreply.github.com>
2023-12-06 08:25:43 +0000

    adjust messaging to reflect stack age

--- packages/repocop/src/remediations/repository-06-topic-monitor-production.ts
+++ packages/repocop/src/remediations/repository-06-topic-monitor-production.ts
@@ -24,7 +24,12 @@ async function notifyOneTeam(
 	const client = new Anghammarad();
 	await client.notify({
 		subject: `Production topic monitoring (for GitHub team ${teamSlug})`,
-		message: `The 'production' topic has applied to ${fullRepoName} as it appears to have a PROD or INFRA stack in AWS. Repositories should have one of the following topics, to help understand what is in production: 'production', 'testing', 'documentation', 'hackday', 'prototype', 'learning', 'interactive'. Visit the links below to learn more about topics and how to add/remove them if you need to.`,
+		message:
+			`The 'production' topic has applied to ${fullRepoName}.` +
+			' This is because it has been associated with a live PROD or INFRA stack that is over three months old.' +
+			' Repositories should have one of the following topics, to help understand what is in production:' +
+			` 'production', 'testing', 'documentation', 'hackday', 'prototype', 'learning', 'interactive'.` +
+			' Visit the links below to learn more about topics and how to add/remove them if you need to.',
 		actions: topicMonitoringProductionTagCtas(fullRepoName, teamSlug),
 		target: { GithubTeamSlug: teamSlug },
 		channel: RequestedChannel.PreferHangouts,
