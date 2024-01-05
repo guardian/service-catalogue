a3c3afcf292df9a094a0fa10bef0aa8ae9a3a4e3
Natasha <67543397+NovemberTang@users.noreply.github.com>
2024-01-05 17:20:03 +0000

    make tests more specific

--- packages/repocop/src/remediations/snyk-integrator/snyk-integrator.test.ts
+++ packages/repocop/src/remediations/snyk-integrator/snyk-integrator.test.ts
@@ -1,9 +1,8 @@
 import { createYaml } from './snyk-integrator';
 
 describe('createYaml', () => {
-	it('should create a yaml file', () => {
-		const yaml = createYaml(['Scala', 'Python']);
-
+	it('should skip node and sbt if no languages are provided', () => {
+		const yaml = createYaml([]);
 		const result = String.raw`name: Snyk
 on:
   push:
@@ -15,13 +14,32 @@ jobs:
     uses: guardian/.github/.github/workflows/sbt-node-snyk.yml@main
     with:
       ORG: <REPLACE ME>
+      SKIP_SBT: true
       SKIP_NODE: true
-      SKIP_PYTHON: false
-      PYTHON_VERSION: <REPLACE ME>
     secrets:
       SNYK_TOKEN: ${'$'}{{ secrets.SNYK_TOKEN }}
 `;
-
+		expect(yaml).toEqual(result);
+	});
+	it('should not skip python and go if they are provided', () => {
+		const yaml = createYaml(['Scala', 'TypeScript', 'Python', 'Go']);
+		const result = String.raw`name: Snyk
+on:
+  push:
+    branches:
+      - main
+  workflow_dispatch: 
+jobs:
+  security:
+    uses: guardian/.github/.github/workflows/sbt-node-snyk.yml@main
+    with:
+      ORG: <REPLACE ME>
+      SKIP_PYTHON: false
+      PYTHON_VERSION: <REPLACE ME>
+      SKIP_GO: false
+    secrets:
+      SNYK_TOKEN: ${'$'}{{ secrets.SNYK_TOKEN }}
+`;
 		expect(yaml).toEqual(result);
 	});
 });
