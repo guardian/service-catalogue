35f9b3920aed7c4f29350df9973377cebd078aa7
Natasha <67543397+NovemberTang@users.noreply.github.com>
2024-01-09 17:03:17 +0000

    create new lambda that does exactly what repocop does

--- packages/cdk/lib/repocop.ts
+++ packages/cdk/lib/repocop.ts
@@ -4,12 +4,13 @@ import type {
 	NoMonitoring,
 } from '@guardian/cdk/lib/constructs/cloudwatch';
 import type { GuStack } from '@guardian/cdk/lib/constructs/core';
+import { GuLambdaFunction } from '@guardian/cdk/lib/constructs/lambda';
 import { Duration } from 'aws-cdk-lib';
 import type { IVpc, SecurityGroup } from 'aws-cdk-lib/aws-ec2';
 import type { Schedule } from 'aws-cdk-lib/aws-events';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import type { DatabaseInstance } from 'aws-cdk-lib/aws-rds';
-import type { Secret } from 'aws-cdk-lib/aws-secretsmanager';
+import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 import type { ITopic, Topic } from 'aws-cdk-lib/aws-sns';
 
 export class Repocop {
@@ -24,7 +25,7 @@ export class Repocop {
 		vpc: IVpc,
 		interactiveMonitorTopic: Topic,
 		dbSecurityGroup: SecurityGroup,
-		githubAppSecret: Secret,
+		repocopGithubSecret: Secret,
 	) {
 		const repocopLampdaProps: GuScheduledLambdaProps = {
 			app: 'repocop',
@@ -39,7 +40,7 @@ export class Repocop {
 				DATABASE_HOSTNAME: cloudqueryDB.dbInstanceEndpointAddress,
 				QUERY_LOGGING: 'false', // Set this to 'true' to enable SQL query logging
 				INTERACTIVE_MONITOR_TOPIC_ARN: interactiveMonitorTopic.topicArn,
-				GITHUB_APP_SECRET: githubAppSecret.secretArn,
+				GITHUB_APP_SECRET: repocopGithubSecret.secretArn,
 				INTERACTIVES_COUNT: guStack.stage === 'PROD' ? '40' : '3',
 			},
 			vpc,
@@ -55,8 +56,41 @@ export class Repocop {
 
 		cloudqueryDB.grantConnect(repocopLambda, 'repocop');
 
-		githubAppSecret.grantRead(repocopLambda);
+		repocopGithubSecret.grantRead(repocopLambda);
 		anghammaradTopic.grantPublish(repocopLambda);
 		interactiveMonitorTopic.grantPublish(repocopLambda);
+
+		const snykIntegratorSecret = new Secret(
+			guStack,
+			`snyk-integrator-github-app-auth`,
+			{
+				secretName: `/${guStack.stage}/${guStack.stack}/service-catalogue/branch-protector-github-app-secret`,
+			},
+		);
+
+		const snykIntegatorLambda: GuLambdaFunction = new GuLambdaFunction(
+			guStack,
+			'snyk-integrator',
+			{
+				app: 'repocop',
+				fileName: 'repocop.zip',
+				handler: 'index.main',
+				memorySize: 1024,
+				runtime: Runtime.NODEJS_20_X,
+				environment: {
+					ANGHAMMARAD_SNS_ARN: anghammaradTopic.topicArn,
+					DATABASE_HOSTNAME: cloudqueryDB.dbInstanceEndpointAddress,
+					QUERY_LOGGING: 'false', // Set this to 'true' to enable SQL query logging
+					INTERACTIVE_MONITOR_TOPIC_ARN: interactiveMonitorTopic.topicArn,
+					GITHUB_APP_SECRET: snykIntegratorSecret.secretArn,
+					INTERACTIVES_COUNT: guStack.stage === 'PROD' ? '40' : '3',
+				},
+				vpc,
+				securityGroups: [dbSecurityGroup],
+				timeout: Duration.minutes(5),
+			},
+		);
+
+		snykIntegratorSecret.grantRead(snykIntegatorLambda);
 	}
 }
