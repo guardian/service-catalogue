73174e0c63ca5e57967e26bc275895af617466a0
Natasha <67543397+NovemberTang@users.noreply.github.com>
2024-01-11 13:33:26 +0000

    verify arrays are not empty before running anything

---
+++ packages/repocop/src/query.test.ts
@@ -0,0 +1,15 @@
+import { toNonEmptyArray } from './query';
+import type { NonEmptyArray } from './query';
+
+describe('Failure on empty arrays', () => {
+	test('test me', () => {
+		const emptyArray: string[] = [];
+		const nonEmptyArray: string[] = ['a', 'b'];
+		const typedNonEmptyArray: NonEmptyArray<string> = ['a', 'b'];
+
+		expect(() => toNonEmptyArray(emptyArray)).toThrow();
+		expect(() => toNonEmptyArray(nonEmptyArray)).not.toThrow();
+		expect(toNonEmptyArray(nonEmptyArray)).toEqual(nonEmptyArray);
+		expect(toNonEmptyArray(nonEmptyArray)).toEqual(typedNonEmptyArray);
+	});
+});
