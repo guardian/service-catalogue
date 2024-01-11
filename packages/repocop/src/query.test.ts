f8b4b88e187a9a50a8d894a8735251af8109cec7
Natasha <67543397+NovemberTang@users.noreply.github.com>
2024-01-11 13:49:49 +0000

    change test name

--- packages/repocop/src/query.test.ts
+++ packages/repocop/src/query.test.ts
@@ -2,7 +2,7 @@ import { toNonEmptyArray } from './query';
 import type { NonEmptyArray } from './query';
 
 describe('Failure on empty arrays', () => {
-	test('test me', () => {
+	test('throw an error if input is an empty array', () => {
 		const emptyArray: string[] = [];
 		const nonEmptyArray: string[] = ['a', 'b'];
 		const typedNonEmptyArray: NonEmptyArray<string> = ['a', 'b'];
