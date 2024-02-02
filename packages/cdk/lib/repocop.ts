4bbf3a9c627a5c3ce66945d237dbfb08161d429b
Natasha <67543397+NovemberTang@users.noreply.github.com>
2024-02-02 14:20:11 +0000

    try json formatted logging

--- packages/cdk/lib/repocop.ts
+++ packages/cdk/lib/repocop.ts
@@ -60,6 +60,7 @@ export class Repocop {
 			vpc,
 			securityGroups: [dbSecurityGroup],
 			timeout: Duration.minutes(5),
+			logFormat: 'JSON',
 		};
 
 		const repocopLambda = new GuScheduledLambda(
