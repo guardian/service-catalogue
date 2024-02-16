11ae13bad641f87bdc69b2fe0e0ff87ba38df3c6
Natasha <67543397+NovemberTang@users.noreply.github.com>
2024-02-16 10:19:57 +0000

    Use official YAML extension

--- packages/snyk-integrator/src/snyk-integrator.ts
+++ packages/snyk-integrator/src/snyk-integrator.ts
@@ -175,9 +175,9 @@ export async function createSnykPullRequest(
 		branchName,
 		changes: [
 			{
-				commitMessage: 'Add Snyk.yml',
+				commitMessage: 'Add Snyk.yaml',
 				files: {
-					'.github/workflows/snyk.yml': snykFileContents,
+					'.github/workflows/snyk.yaml': snykFileContents,
 				},
 			},
 		],
