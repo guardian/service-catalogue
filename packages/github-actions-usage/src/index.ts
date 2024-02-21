147a35fa2333705987d9c7c116ad66f41c549c9b
akash1810 <akash1810@users.noreply.github.com>
2024-02-21 07:36:35 +0000

    feat(github-actions-usage): Add Prisma querying

--- packages/github-actions-usage/src/index.ts
+++ packages/github-actions-usage/src/index.ts
@@ -1,3 +1,18 @@
-export function main() {
-	console.log('Hello, world!');
+import { getPrismaClient } from 'common/database';
+import { getConfig } from './config';
+
+export async function main() {
+	const config = await getConfig();
+	const prismaClient = getPrismaClient(config);
+
+	const workflows = await prismaClient.github_workflows.findMany({
+		select: {
+			contents: true,
+		},
+		take: 10,
+	});
+
+	workflows.forEach(({ contents }) => {
+		console.log(contents);
+	});
 }
