5a87904f01c768c3defaf2f1c3f98b74e0c10bcc
akash1810 <akash1810@users.noreply.github.com>
2024-02-21 07:27:47 +0000

    feat(github-actions-usage): Initial structure

---
+++ packages/github-actions-usage/src/run-locally.ts
@@ -0,0 +1,9 @@
+import { config } from 'dotenv';
+import { main } from './index';
+
+// Read the .env file from the repository root
+config({ path: `../../.env` });
+
+if (require.main === module) {
+	void main();
+}
