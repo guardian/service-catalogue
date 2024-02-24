9e1e3b58fa82af62750864af46632ff5fc9f71ae
akash1810 <akash1810@users.noreply.github.com>
2024-02-24 17:21:16 +0000

    test(github-actions-usage): Add tests for `validateRawWorkflow`

--- packages/github-actions-usage/src/transform.test.ts
+++ packages/github-actions-usage/src/transform.test.ts
@@ -1,5 +1,7 @@
+import { stripMargin } from 'common/src/string';
+import type { RawGithubWorkflow } from './db-read';
 import type { GithubWorkflowFile } from './transform';
-import { getUsesStringsFromWorkflow } from './transform';
+import { getUsesStringsFromWorkflow, validateRawWorkflow } from './transform';
 
 describe('getUsesStringsFromWorkflow', () => {
 	test('Workflow with single job, and multiple steps', () => {
@@ -63,5 +65,92 @@ describe('getUsesStringsFromWorkflow', () => {
 			'./.github/actions/setup-node-env',
 			'./.github/workflows/chromatic.yml',
 		]);
+	});
+});
+
+describe('validateRawWorkflow', () => {
+	test('return value when content is valid', () => {
+		const workflow: RawGithubWorkflow = {
+			full_name: 'guardian/fake-repo',
+			path: '.github/workflows/ci.yml',
+			contents: stripMargin`
+				|name: Snyk
+				|on:
+				|  push:
+				|    branches:
+				|      - main
+				|  workflow_dispatch:
+				|
+				|jobs:
+				|  security:
+				|    uses: guardian/.github/.github/workflows/sbt-node-snyk.yml@main
+				|    with:
+				|      ORG: guardian-devtools
+				|      SKIP_SBT: true
+				|    secrets:
+				|      SNYK_TOKEN: \${{ secrets.SNYK_TOKEN }}
+				|`,
+		};
+		expect(validateRawWorkflow(workflow)).toEqual({
+			repository: 'guardian/fake-repo',
+			path: '.github/workflows/ci.yml',
+			content: {
+				name: 'Snyk',
+				on: {
+					push: {
+						branches: ['main'],
+					},
+					workflow_dispatch: null,
+				},
+				jobs: {
+					security: {
+						uses: 'guardian/.github/.github/workflows/sbt-node-snyk.yml@main',
+						with: {
+							ORG: 'guardian-devtools',
+							SKIP_SBT: true,
+						},
+						secrets: {
+							SNYK_TOKEN: '${{ secrets.SNYK_TOKEN }}',
+						},
+					},
+				},
+			},
+		});
+	});
+
+	test('return value when content is YAML, but not a valid GitHub Workflow', () => {
+		const workflow: RawGithubWorkflow = {
+			full_name: 'guardian/fake-repo',
+			path: '.github/workflows/ci.yml',
+			contents: stripMargin`
+				|name: Snyk
+				|on:
+				|  push:
+				|    branches:
+				|      - main
+				|  workflow_dispatch:
+				|
+				|fakeKey: fakeValue
+				|
+				|jobs:
+				|  security:
+				|    uses: guardian/.github/.github/workflows/sbt-node-snyk.yml@main
+				|    with:
+				|      ORG: guardian-devtools
+				|      SKIP_SBT: true
+				|    secrets:
+				|      SNYK_TOKEN: \${{ secrets.SNYK_TOKEN }}
+				|`,
+		};
+		expect(validateRawWorkflow(workflow)).toBeUndefined();
+	});
+
+	test('return value when content is not YAML', () => {
+		const workflow: RawGithubWorkflow = {
+			full_name: 'guardian/fake-repo',
+			path: '.github/workflows/ci.yml',
+			contents: 'I am not YAML',
+		};
+		expect(validateRawWorkflow(workflow)).toBeUndefined();
 	});
 });
