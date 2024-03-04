c7e05699806bcfcaa9168292482f5ea4300cc3cb
Chris Jones <chrislomaxjones96@gmail.com>
2024-03-04 12:20:20 +0000

    Update migration to make column names clearer
    Co-authored-by: Natasha <67543397+NovemberTang@users.noreply.github.com>
    Co-authored-by: Akash Askoolum <akash1810@users.noreply.github.com>
    Co-authored-by: TJ Silver <15648334+tjsilver@users.noreply.github.com>

--- packages/common/prisma/views/public/view_repo_ownership.sql
+++ packages/common/prisma/views/public/view_repo_ownership.sql
@@ -1,14 +1,13 @@
 SELECT
   ght.id AS github_team_id,
   ght.name AS github_team_name,
-  tr.full_name AS repo_name,
-  tr.full_name,
+  ght.slug AS github_team_slug,
+  tr.name AS short_repo_name,
+  tr.full_name AS full_repo_name,
   tr.role_name,
   tr.archived,
   gtt.team_name AS galaxies_team,
-  gtt.team_contact_email,
-  ght.slug AS github_team_slug,
-  tr.name
+  gtt.team_contact_email
 FROM
   (
     (
