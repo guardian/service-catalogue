9b258fc39a236791256733f71cc53747f8940a65
TJ Silver <thalia.silver@guardian.co.uk>
2024-03-14 12:46:00 +0000

    feat: create repocop vulns and write to table

--- packages/repocop/src/index.ts
+++ packages/repocop/src/index.ts
@@ -2,6 +2,7 @@ import { CloudWatchClient } from '@aws-sdk/client-cloudwatch';
 import type {
 	PrismaClient,
 	repocop_github_repository_rules,
+	repocop_vulnerabilities,
 } from '@prisma/client';
 import { awsClientConfig } from 'common/aws';
 import { getPrismaClient } from 'common/database';
@@ -28,11 +29,7 @@ import { sendUnprotectedRepo } from './remediation/snyk-integrator/send-to-sns';
 import { sendPotentialInteractives } from './remediation/topics/topic-monitor-interactive';
 import { applyProductionTopicAndMessageTeams } from './remediation/topics/topic-monitor-production';
 import { createAndSendVulnerabilityDigests } from './remediation/vuln-digest/vuln-digest';
-import type {
-	AwsCloudFormationStack,
-	EvaluationResult,
-	RepocopVulnerability,
-} from './types';
+import type { AwsCloudFormationStack, EvaluationResult } from './types';
 
 async function writeEvaluationTable(
 	evaluatedRepos: repocop_github_repository_rules[],
@@ -47,6 +44,21 @@ async function writeEvaluationTable(
 	});
 
 	console.log('Finished writing to table');
+}
+
+async function writeVulnerabilitiesTable(
+	vulnerabilities: repocop_vulnerabilities[],
+	prisma: PrismaClient,
+) {
+	console.log('Clearing the vulnerabilities table');
+	await prisma.repocop_vulnerabilities.deleteMany({});
+
+	console.log(`Writing ${vulnerabilities.length} vulnerabilities to table`);
+	await prisma.repocop_vulnerabilities.createMany({
+		data: vulnerabilities,
+	});
+
+	console.log('Finished writing to vulnerabilities table');
 }
 
 export async function main() {
@@ -80,14 +92,15 @@ export async function main() {
 	);
 
 	const repocopRules = evaluationResults.map((r) => r.repocopRules);
-	const severityPredicate = (x: RepocopVulnerability) => x.severity === 'high';
+	const severityPredicate = (x: repocop_vulnerabilities) =>
+		x.severity === 'high';
 	const [high, critical] = partition(
 		evaluationResults.flatMap((r) => r.vulnerabilities),
 		severityPredicate,
 	);
 
-	const highPatchable = high.filter((x) => x.isPatchable).length;
-	const criticalPatchable = critical.filter((x) => x.isPatchable).length;
+	const highPatchable = high.filter((x) => x.is_patchable).length;
+	const criticalPatchable = critical.filter((x) => x.is_patchable).length;
 
 	console.warn(
 		`Found ${high.length} out of date high vulnerabilities, of which ${highPatchable} are patchable`,
@@ -95,6 +108,27 @@ export async function main() {
 	console.warn(
 		`Found ${critical.length} out of date critical vulnerabilities, of which ${criticalPatchable} are patchable`,
 	);
+
+	/**
+	 * Create repocop vulnerabilities and write to repocop_vulnerabilities table
+	 */
+	const vulnerabilities: repocop_vulnerabilities[] = evaluationResults
+		.map((result) => result.vulnerabilities)
+		.flat()
+		.map((vuln) => {
+			const owners = repoOwners.filter(
+				(owner) => vuln.full_name === owner.full_repo_name,
+			);
+			return owners.length > 0
+				? owners.map((owner) => ({
+						...vuln,
+						repo_owner: owner.github_team_slug,
+					}))
+				: { ...vuln, repo_owner: 'unknown' };
+		})
+		.flat() as unknown as repocop_vulnerabilities[];
+
+	await writeVulnerabilitiesTable(vulnerabilities, prisma);
 
 	const awsConfig = awsClientConfig(config.stage);
 	const cloudwatch = new CloudWatchClient(awsConfig);
