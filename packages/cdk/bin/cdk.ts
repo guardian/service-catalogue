4910a642c1c985a9d5ad54c8c76d86d20bf66d2d
akash1810 <akash1810@users.noreply.github.com>
2024-03-15 10:08:38 +0000

    wip: Generate infrastructure diagram using `@aws/pdk`
    See https://aws.github.io/aws-pdk/developer_guides/cdk-graph-plugin-diagram/index.html.

--- packages/cdk/bin/cdk.ts
+++ packages/cdk/bin/cdk.ts
@@ -1,6 +1,8 @@
 import 'source-map-support/register';
+import { CdkGraph } from '@aws/pdk/cdk-graph';
+import { CdkGraphDiagramPlugin } from '@aws/pdk/cdk-graph-plugin-diagram';
 import { GuRoot } from '@guardian/cdk/lib/constructs/root';
-import { Duration } from 'aws-cdk-lib';
+import { App, Duration } from 'aws-cdk-lib';
 import { Schedule } from 'aws-cdk-lib/aws-events';
 import { ServiceCatalogue } from '../lib/service-catalogue';
 
@@ -21,3 +23,34 @@ new ServiceCatalogue(app, 'ServiceCatalogue-CODE', {
 	rdsDeletionProtection: false,
 	cloudFormationStackName: 'deploy-CODE-service-catalogue',
 });
+
+/*
+Uses `aws/pdk` to generate a graph of the CDK application.
+The diagram plugin must be wrapped in an async IFEE.
+
+`CdkGraph` creates a diagram for each node attached to `App`.
+In the `GuRoot` above, there are two nodes, differing only by `stage`.
+This creates a rather busy diagram, with duplicated information.
+As a workaround, we create a new `App` and attach only one `ServiceCatalogue` instance to it.
+To avoid it causing issues with the rest of the application, we set the `outdir` to a different directory.
+
+See https://aws.github.io/aws-pdk/developer_guides/cdk-graph-plugin-diagram/index.html.
+ */
+void (async () => {
+	const appForGraphing = new App({ outdir: 'cdk.out/graph' });
+	new ServiceCatalogue(
+		appForGraphing,
+		'ServiceCatalogue-PROD-for-diagramming',
+		{
+			stack: 'deploy',
+			stage: 'PROD',
+			env: { region: 'eu-west-1' },
+			cloudFormationStackName: 'deploy-PROD-service-catalogue',
+		},
+	);
+	const graph = new CdkGraph(appForGraphing, {
+		plugins: [new CdkGraphDiagramPlugin()],
+	});
+	appForGraphing.synth();
+	await graph.report();
+})();
