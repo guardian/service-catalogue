e29d4ba7c82f1ae6c7edc1aab55f35291cb07902
Chris Jones <chrislomaxjones96@gmail.com>
2024-03-25 14:17:58 +0000

    Add task definition for performing Prisma migrations

--- packages/cdk/lib/cloudquery/images.ts
+++ packages/cdk/lib/cloudquery/images.ts
@@ -9,12 +9,15 @@ export const Images = {
 	amazonLinux: ContainerImage.fromRegistry(
 		'public.ecr.aws/amazonlinux/amazonlinux:latest',
 	),
-
 	// https://github.com/guardian/cq-source-ns1
 	ns1Source: ContainerImage.fromRegistry(
 		`ghcr.io/guardian/cq-source-ns1:${Versions.CloudqueryNs1}`,
 	),
 	postgres: ContainerImage.fromRegistry(
 		'public.ecr.aws/docker/library/postgres:16-alpine',
+	),
+	prismaMigrate: ContainerImage.fromRegistry(
+		// TODO: Change to the desired SHA once PR is merged into main
+		'ghcr.io/guardian/service-catalogue/prisma-migrate:1',
 	),
 };
