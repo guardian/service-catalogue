ddb1655746f3a895e9fae0c4d112b022f074ba32
Chris Jones <chrislomaxjones96@gmail.com>
2024-03-27 17:09:00 +0000

    Add a comment about prisma-migrate image SHAs

--- packages/cdk/lib/cloudquery/images.ts
+++ packages/cdk/lib/cloudquery/images.ts
@@ -16,6 +16,14 @@ export const Images = {
 	postgres: ContainerImage.fromRegistry(
 		'public.ecr.aws/docker/library/postgres:16-alpine',
 	),
+	/**
+	 * This image is built in CI by the service-catalogue, and tagged with the
+	 * SHA of the corresponding commit
+	 *
+	 * To use a new image, update the SHA here. The full set of possible tags
+	 * can be found at
+	 * 	https://github.com/guardian/service-catalogue/pkgs/container/service-catalogue%2Fprisma-migrate
+	 */
 	prismaMigrate: ContainerImage.fromRegistry(
 		'ghcr.io/guardian/service-catalogue/prisma-migrate:sha-06b60dc3f47bfdfe8997d3d622e91f5508733ece',
 	),
