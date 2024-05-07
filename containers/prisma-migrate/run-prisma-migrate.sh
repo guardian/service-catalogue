e636b42d8973e63161a022e969bd21e460d84af2
Natasha <67543397+NovemberTang@users.noreply.github.com>
2024-05-07 13:24:33 +0100

    Make prisma container readonly (#986)
    * move prisma to mountable folder
    
    * make prisma container readonly. add writeable mount

--- containers/prisma-migrate/run-prisma-migrate.sh
+++ containers/prisma-migrate/run-prisma-migrate.sh
@@ -3,13 +3,13 @@
 set -e
 
 echo 'Retrieving Prisma artifact from S3'
-aws s3 cp "s3://$ARTIFACT_BUCKET/$PRISMA_ARTIFACT_KEY" ./
+aws s3 cp "s3://$ARTIFACT_BUCKET/$PRISMA_ARTIFACT_KEY" ./prisma/
 
 echo 'Unzipping Prisma artifact'
-unzip -q prisma.zip
+unzip -q prisma/prisma.zip
 
 DB_PORT=5432
 export DATABASE_URL=postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/postgres
 
 echo 'Running prisma migrate deploy'
-node_modules/.bin/prisma migrate deploy
+prisma/node_modules/.bin/prisma migrate deploy
