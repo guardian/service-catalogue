af332bf7adc04e3d0fe3f09e137567f88891530b
Julia Branke <Julia.Branke@guardian.co.uk>
2024-05-09 11:38:34 +0100

    Mark aws manage iam policies as taggable false

--- packages/common/prisma/migrations/20240508195000_refresh_aws_resources/migrations.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE FUNCTION refresh_aws_resources()
-    RETURNS TEXT AS
-$$
-BEGIN
-    REFRESH MATERIALIZED VIEW aws_resources WITH DATA;
-    RETURN 'Triggered refresh of materialized view aws_resources';
-END;
-$$
-    LANGUAGE plpgsql;
