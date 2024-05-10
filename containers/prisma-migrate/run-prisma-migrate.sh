8b30965d38838f3525a0bebcbfd107855d0f4b33
Julia Branke <Julia.Branke@guardian.co.uk>
2024-05-10 20:03:20 +0100

    change path in run-prisma-migrate.sh and log it in case I get it wrong

--- containers/prisma-migrate/run-prisma-migrate.sh
+++ containers/prisma-migrate/run-prisma-migrate.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-# CI will build a new image with the code here, but you need to update the image used
-# in packages/cdk/lib/cloudquery/image.ts to use the new image.
 
 set -e
 
 echo 'Run run-prisma-migrate.sh'
 
-echo 'Step1: Retrieving Prisma artifact from S3'
+echo 'Step 1: Retrieving Prisma artifact from S3'
 aws s3 cp "s3://$ARTIFACT_BUCKET/$PRISMA_ARTIFACT_KEY" ./prisma/
 
-echo 'Step2: Unzipping Prisma artifact'
+echo 'Step 2: Unzipping Prisma artifact'
 unzip -q prisma/prisma.zip
 
 DB_PORT=5432
 export DATABASE_URL=postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/postgres
 
-echo 'Step3: Running prisma migrate deploy'
-prisma/node_modules/.bin/prisma migrate deploy
+echo 'where is prisma'
+which prisma
+
+echo 'Step 3: Running prisma migrate deploy'
+./node_modules/.bin/prisma migrate deploy
