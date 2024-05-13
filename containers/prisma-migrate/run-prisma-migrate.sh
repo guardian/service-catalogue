e4878a0ec5eb50969faddcaf3b48d804652923b9
akash1810 <akash1810@users.noreply.github.com>
2024-05-13 12:37:47 +0100

    fix(prisma): Use full file paths
    To increase determinism, use full file paths.
    
    By not using relative paths, we remove the need for the working directory to be correct.
    
    Co-authored-by: Julia <JuliaBrigitte@users.noreply.github.com>
    Co-authored-by: Natasha <NovemberTang@users.noreply.github.com>

--- containers/prisma-migrate/run-prisma-migrate.sh
+++ containers/prisma-migrate/run-prisma-migrate.sh
@@ -2,14 +2,17 @@
 
 set -e
 
+# This must match the WORKDIR in the Dockerfile
+ROOT_DIR=/usr/src/app
+
 echo 'Retrieving Prisma artifact from S3'
-aws s3 cp "s3://$ARTIFACT_BUCKET/$PRISMA_ARTIFACT_KEY" ./prisma/
+aws s3 cp "s3://$ARTIFACT_BUCKET/$PRISMA_ARTIFACT_KEY" "${ROOT_DIR}/prisma/"
 
 echo 'Unzipping Prisma artifact'
-unzip -q prisma/prisma.zip
+unzip -q "${ROOT_DIR}/prisma/prisma.zip"
 
 DB_PORT=5432
 export DATABASE_URL=postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/postgres
 
 echo 'Running prisma migrate deploy'
-prisma/node_modules/.bin/prisma migrate deploy
+"${ROOT_DIR}/node_modules/.bin/prisma" migrate deploy
