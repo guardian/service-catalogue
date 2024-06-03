bc9f47b9547961c588d632b0ee536a6dc489a4de
Ashleigh Carr <ashcorr20@gmail.com>
2024-06-03 16:38:50 +0100

    Prisma requires all tables to have atleast 1 index, so lets create a few

--- packages/common/prisma/migrations/20240530103800_obligatron_results/migration.sql
+++ packages/common/prisma/migrations/20240530103800_obligatron_results/migration.sql
@@ -6,3 +6,7 @@ CREATE TABLE IF NOT EXISTS obligatron_results (
     "contacts" JSONB NOT NULL,
     "url" TEXT NOT NULL
 );
+
+CREATE INDEX IF NOT EXISTS "obligatron_results_date_idx" ON "obligatron_results" ("date");
+CREATE INDEX IF NOT EXISTS "obligatron_results_obligation_name_idx" ON "obligatron_results" ("obligation_name");
+CREATE INDEX IF NOT EXISTS "obligatron_results_resource_idx" ON "obligatron_results" ("resource");
