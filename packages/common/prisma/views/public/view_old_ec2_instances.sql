2303a7bb8b476b85e7cb65abcceb17c989ff02e8
akash1810 <akash1810@users.noreply.github.com>
2024-06-18 10:04:55 +0100

    fix(prisma): Updates after introspection
    Update Prisma's `views` directory, following #1066,
    via an introspection.
    
    See https://www.prisma.io/docs/orm/prisma-schema/introspection.

--- packages/common/prisma/views/public/view_old_ec2_instances.sql
+++ packages/common/prisma/views/public/view_old_ec2_instances.sql
@@ -1,31 +1,24 @@
 SELECT
-  ac.id AS account_id,
-  ac.name AS account_name,
-  ec2.instance_id,
-  (ec2.state ->> 'Name' :: text) AS state,
-  (ec2.tags ->> 'Stack' :: text) AS stack,
-  (ec2.tags ->> 'Stage' :: text) AS stage,
-  (ec2.tags ->> 'App' :: text) AS app,
-  (ec2.tags ->> 'gu:repo' :: text) AS repo,
-  ec2.region,
-  coalesce_dates(img.creation_date, ec2.launch_time) AS creation_or_launch_time
+  fn_old_ec2_instances.account_id,
+  fn_old_ec2_instances.account_name,
+  fn_old_ec2_instances.instance_id,
+  fn_old_ec2_instances.state,
+  fn_old_ec2_instances.stack,
+  fn_old_ec2_instances.stage,
+  fn_old_ec2_instances.app,
+  fn_old_ec2_instances.repo,
+  fn_old_ec2_instances.region,
+  fn_old_ec2_instances.creation_or_launch_time
 FROM
-  (
-    (
-      aws_ec2_instances ec2
-      LEFT JOIN aws_ec2_images img ON ((ec2.image_id = img.image_id))
-    )
-    LEFT JOIN aws_accounts ac ON ((ec2.account_id = ac.id))
-  )
-WHERE
-  (
-    (
-      (
-        coalesce_dates(img.creation_date, ec2.launch_time) IS NULL
-      )
-      OR (
-        coalesce_dates(img.creation_date, ec2.launch_time) < (NOW() - '30 days' :: INTERVAL)
-      )
-    )
-    AND ((ec2.state ->> 'Name' :: text) = 'running' :: text)
+  fn_old_ec2_instances() fn_old_ec2_instances(
+    account_id,
+    account_name,
+    instance_id,
+    state,
+    stack,
+    stage,
+    app,
+    repo,
+    region,
+    creation_or_launch_time
   );
