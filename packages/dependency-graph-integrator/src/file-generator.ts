85da3d162eddc907cc166daa559db89bdcaec22c
TJ Silver <thalia.silver@guardian.co.uk>
2024-06-21 15:25:09 +0100

    refactor: use SHAs for action versions in dependency-graph-integrator

--- packages/dependency-graph-integrator/src/file-generator.ts
+++ packages/dependency-graph-integrator/src/file-generator.ts
@@ -15,8 +15,12 @@ export function createYaml(prBranch: string): string {
 			'dependency-graph': {
 				// 'runs-on': 'ubuntu-latest', //let's see how we do without this
 				steps: [
-					{ uses: 'actions/checkout@v4' },
-					{ uses: 'scalacenter/sbt-dependency-submission@v2' },
+					{
+						uses: 'actions/checkout@692973e3d937129bcbf40652eb9f2f61becf3332 # v4.1.7',
+					},
+					{
+						uses: 'scalacenter/sbt-dependency-submission@7ebd561e5280336d3d5b445a59013810ff79325e # v3.0.1',
+					},
 				],
 				permissions: { contents: 'write' },
 			},
