c4dd9edb24885092b08c86d6610a1b88447365c1
TJ Silver <thalia.silver@guardian.co.uk>
2024-06-21 15:52:27 +0100

    fix: refer to Github not Snyk

--- packages/dependency-graph-integrator/src/file-generator.ts
+++ packages/dependency-graph-integrator/src/file-generator.ts
@@ -56,7 +56,7 @@ export function generatePrBody(branchName: string): string {
 			'If a repository is in production, we need to track its third party dependencies for vulnerabilities. ' +
 				'Historically, we have done this using Snyk, but we are now moving to GitHub’s native Dependabot. ' +
 				'Scala is not a language that Dependabot supports out of the box, this workflow is required to make it happen' +
-				'As a result, we have raised this PR on your behalf to add it to Snyk.',
+				'As a result, we have raised this PR on your behalf to add it to the Dependency Graph.',
 		),
 		h2('How has it been verified?'),
 		p(
