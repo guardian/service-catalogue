1baac473bb427bf0caf4fd92854ce77d01fe71cf
TJ Silver <thalia.silver@guardian.co.uk>
2024-06-24 17:05:34 +0100

    refactor: setup dependency graph integrator event with correct params
    Co-authored-by: Natasha <67543397+NovemberTang@users.noreply.github.com>

--- packages/dependency-graph-integrator/src/index.ts
+++ packages/dependency-graph-integrator/src/index.ts
@@ -14,17 +14,24 @@ export async function main(event: DependencyGraphIntegratorEvent) {
 	const config: Config = getConfig();
 	const branch = generateBranchName('sbt-dependency-graph');
 
+	const boardNumber = 110;
+	const author = 'gu-snyk-integrator'; // TODO: create new 'gu-dependency-graph-integrator' app
+	const title =
+		'Submit sbt dependencies to GitHub for vulnerability monitoring';
+	const fileName = '.github/workflows/sbt-dependency-graph.yaml';
+	const commitMessage = 'Add sbt-dependency-graph.yaml';
+
 	await createPrAndAddToProject(
 		config.stage,
 		event.name,
-		'?????', //TODO - add author
+		author,
 		branch,
-		'Submit sbt dependencies to GitHub for vulnerability monitoring',
+		title,
 		generatePrBody(branch),
-		'.github/workflows/sbt-dependency-graph.yml',
+		fileName,
 		createYaml(branch),
-		'Add sbt-dependency-graph.yml',
-		NaN, //TODO - add board number
+		commitMessage,
+		boardNumber,
 	);
 }
 
