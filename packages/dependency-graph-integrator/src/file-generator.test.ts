0875a1cde325e38c03d7307f9d6597f39f8f5020
Natasha <67543397+NovemberTang@users.noreply.github.com>
2024-06-26 12:15:18 +0100

    print out the snapshot file as part of the action

--- packages/dependency-graph-integrator/src/file-generator.test.ts
+++ packages/dependency-graph-integrator/src/file-generator.test.ts
@@ -3,7 +3,8 @@ import { createYaml } from './file-generator';
 describe('createYaml', () => {
 	it('should generate the following yaml file', () => {
 		const yaml = createYaml('branch');
-		const result = String.raw`name: Update Dependency Graph for SBT
+		const result =
+			String.raw`name: Update Dependency Graph for SBT
 on:
   push:
     branches:
@@ -15,9 +16,16 @@ jobs:
     runs-on: ubuntu-latest
     steps:
       - name: Checkout branch
+        id: checkout
         uses: actions/checkout@692973e3d937129bcbf40652eb9f2f61becf3332 # v4.1.7
       - name: Submit dependencies
+        id: submit
         uses: scalacenter/sbt-dependency-submission@7ebd561e5280336d3d5b445a59013810ff79325e # v3.0.1
+      - name: Log snapshot for user validation
+        id: validate
+        run: cat` +
+			' ${{ steps.submit.outputs.snapshot-json-path }}' + // Need to split this line to avoid syntax errors due to the template string
+			String.raw`
     permissions:
       contents: write
 `;
