78d7672c138e437bc09ef48568c8347e75f76a91
Mario Savarese <mario.savarese@guardian.co.uk>
2024-07-04 12:24:35 +0100

    Tidy up

--- packages/cloudbuster/src/findings.ts
+++ packages/cloudbuster/src/findings.ts
@@ -1,6 +1,9 @@
 import type { aws_securityhub_findings, PrismaClient } from '@prisma/client';
-import type { Finding, SecurityHubSeverity } from './types';
+import type { FsbpDigest, SecurityHubSeverity } from './types';
 
+/**
+ * Determines whether a Security Hub finding is within the SLA window
+ */
 function isWithinSlaTime(finding: aws_securityhub_findings): boolean {
 	if (!finding.first_observed_at) {
 		return false;
@@ -9,12 +12,18 @@ function isWithinSlaTime(finding: aws_securityhub_findings): boolean {
 	const today = new Date();
 	const timeDifference = today.getTime() - finding.first_observed_at.getTime();
 	const dayDifference = timeDifference / (1000 * 60 * 60 * 24);
+
 	const isWithinTwoDays = Math.abs(dayDifference) <= 2;
 	const isWithinThirtyDays = Math.abs(dayDifference) <= 30;
 
-	const severity = (finding.severity as { Label: SecurityHubSeverity })[
-		'Label'
-	];
+	let severity;
+	if (
+		finding.severity &&
+		typeof finding.severity === 'object' &&
+		'Label' in finding.severity
+	) {
+		severity = finding.severity['Label'];
+	}
 
 	return (
 		(severity === 'CRITICAL' && isWithinTwoDays) ||
@@ -22,10 +31,51 @@ function isWithinSlaTime(finding: aws_securityhub_findings): boolean {
 	);
 }
 
+/**
+ * Transforms a SQL row into a vulnerability digest
+ */
+function fsbpDigestFromFinding(finding: aws_securityhub_findings): FsbpDigest {
+	let severity = null;
+	let remediationUrl = null;
+
+	if (
+		finding.severity &&
+		typeof finding.severity === 'object' &&
+		'Label' in finding.severity
+	) {
+		severity = finding.severity['Label'] as SecurityHubSeverity;
+	}
+
+	if (finding.remediation && typeof finding.remediation === 'object') {
+		const recommendation = finding.remediation as {
+			Recommendation: {
+				Url: string | null;
+			};
+		} | null;
+		if (recommendation) {
+			if (
+				'Url' in recommendation['Recommendation'] &&
+				recommendation['Recommendation']['Url']
+			) {
+				remediationUrl = recommendation['Recommendation']['Url'];
+			}
+		}
+	}
+
+	return {
+		awsAccountName: finding.aws_account_name,
+		title: finding.title,
+		severity,
+		remediationUrl: remediationUrl,
+		firstObservedAt: finding.first_observed_at,
+		isWithinSla: isWithinSlaTime(finding),
+	};
+}
+
 export async function getFsbpFindings(
 	prisma: PrismaClient,
 	severities: SecurityHubSeverity[],
-): Promise<Finding[]> {
+): Promise<FsbpDigest[]> {
 	const findings = await prisma.aws_securityhub_findings.findMany({
 		where: {
 			OR: severities.map((s) => ({
@@ -33,17 +83,9 @@ export async function getFsbpFindings(
 			})),
 		},
 	});
-	const fspb = findings.map((f) => {
-		const severity = f.severity as { Label: SecurityHubSeverity };
 
-		return {
-			accountName: f.aws_account_name,
-			title: f.title,
-			severity: severity['Label'],
-			withinSlaTime: isWithinSlaTime(f),
-		} as Finding;
-	});
+	const digests = findings.map(fsbpDigestFromFinding);
 
-	console.log(fspb);
-	return fspb;
+	console.log(digests);
+	return digests;
 }
--- packages/cloudbuster/src/types.ts
+++ packages/cloudbuster/src/types.ts
@@ -1,13 +1,15 @@
+export interface FsbpDigest {
+	awsAccountName: string | null;
+	title: string;
+	remediationUrl: string | null;
+	severity: SecurityHubSeverity | null;
+	firstObservedAt: Date | null;
+	isWithinSla: boolean;
+}
+
 export type SecurityHubSeverity =
 	| 'CRITICAL'
 	| 'HIGH'
 	| 'INFORMATION'
 	| 'LOW'
 	| 'MEDIUM';
-
-export type Finding = {
-	accountName: string;
-	title: string;
-	severity: SecurityHubSeverity;
-	withinSlaTime: boolean;
-};
