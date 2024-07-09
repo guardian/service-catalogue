2a5eda778fad23a99527cb90e5cf2eeaee706931
Mario Savarese <mario.savarese@guardian.co.uk>
2024-07-10 00:08:59 +0100

    Move severities to config, tidy up even more

---
+++ packages/cloudbuster/src/digests.ts
@@ -0,0 +1,48 @@
+import { groupFindingsByAccount } from './findings';
+import type { Digest, Finding, GroupedFindings } from './types';
+
+/**
+ * Given a list of findings, create a list of digests ready to be emailed out
+ */
+export function createDigestsFromFindings(findings: Finding[]): Digest[] {
+	const groupedFindings = groupFindingsByAccount(findings);
+
+	return Object.keys(groupedFindings)
+		.map((awsAccountId) =>
+			createDigestForAccount(awsAccountId, groupedFindings),
+		)
+		.filter((d) => d !== undefined);
+}
+
+function createDigestForAccount(
+	awsAccountId: string,
+	findings: GroupedFindings,
+): Digest | undefined {
+	const teamFindings = findings[awsAccountId];
+
+	if (!teamFindings || teamFindings.length == 0) {
+		return undefined;
+	}
+
+	return {
+		accountId: awsAccountId,
+		subject: `Security Hub vulnerabilities detected in AWS account ${teamFindings[0]?.awsAccountName}`,
+		message: createEmailBody(teamFindings),
+	};
+}
+
+function createEmailBody(findings: Finding[]): string {
+	const findingsSortedByPriority = findings.sort(
+		(a, b) => (b.priority ?? 0) - (a.priority ?? 0),
+	);
+
+	return `The following vulnerabilities have been found in your account\n: 
+        ${findingsSortedByPriority
+					.map(
+						(f) =>
+							`[${f.severity}] ${f.title}
+Affected resource(s): ${f.resources.join(',')}
+Remediation: ${f.remediationUrl ?? 'Unknown'}`,
+					)
+					.join('\n\n')}`;
+}
--- packages/cloudbuster/src/index.ts
+++ packages/cloudbuster/src/index.ts
@@ -1,22 +1,14 @@
 import { getPrismaClient } from 'common/database';
 import { getConfig } from './config';
-import { createDigestForAccount } from './digest';
-import { getFsbpFindings, groupFindingsByAccount } from './findings';
-import type { Digest, SecurityHubSeverity } from './types';
+import { createDigestsFromFindings } from './digests';
+import { getFsbpFindings } from './findings';
 
 export async function main() {
 	const config = await getConfig();
 	const prisma = getPrismaClient(config);
 
-	const severities: SecurityHubSeverity[] = ['CRITICAL', 'HIGH'];
-	const findings = await getFsbpFindings(prisma, severities);
-	const groupedFindings = groupFindingsByAccount(findings);
-
-	const digests = Object.keys(groupedFindings)
-		.map((awsAccountId) =>
-			createDigestForAccount(awsAccountId, groupedFindings),
-		)
-		.filter(Boolean) as Digest[];
+	const findings = await getFsbpFindings(prisma, config.severities);
+	const digests = createDigestsFromFindings(findings);
 
 	for (const digest of digests) {
 		console.log({ digest });
