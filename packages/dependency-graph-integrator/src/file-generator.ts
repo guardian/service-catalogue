abacc14abfae143df70e2f0e1444069eadf3581b
TJ Silver <thalia.silver@guardian.co.uk>
2024-08-01 17:50:36 +0100

    refactor: split out yaml steps for each language

--- packages/dependency-graph-integrator/src/file-generator.ts
+++ packages/dependency-graph-integrator/src/file-generator.ts
@@ -3,16 +3,60 @@ import type { DepGraphLanguage } from 'common/types';
 import { h2, p, tsMarkdown } from 'ts-markdown';
 import { stringify } from 'yaml';
 
-const workflowsForLanguages = {
-	Scala:
-		'scalacenter/sbt-dependency-submission@7ebd561e5280336d3d5b445a59013810ff79325e # v3.0.1',
-	Kotlin:
-		'gradle/actions/dependency-submission@d9c87d481d55275bb5441eef3fe0e46805f9ef70 # v3.5.0',
-};
+function createYamlLanguageSteps(
+	repo: string,
+): Record<DepGraphLanguage, ConcatArray<object>> {
+	return {
+		Scala: [
+			{
+				name: 'Checkout branch',
+				id: 'checkout',
+				uses: 'actions/checkout@692973e3d937129bcbf40652eb9f2f61becf3332 # v4.1.7',
+			},
+			{
+				name: 'Submit dependencies',
+				id: 'submit',
+				uses: 'scalacenter/sbt-dependency-submission@7ebd561e5280336d3d5b445a59013810ff79325e # v3.0.1',
+			},
+			{
+				name: 'Log snapshot for user validation',
+				id: 'validate',
+				run: 'cat ${{ steps.submit.outputs.snapshot-json-path }} | jq',
+			},
+		],
+		Kotlin: [
+			{
+				name: 'Checkout branch',
+				id: 'checkout',
+				uses: 'actions/checkout@692973e3d937129bcbf40652eb9f2f61becf3332 # v4.1.7',
+			},
+			{
+				name: 'Set up Java',
+				id: 'setup',
+				uses: 'actions/setup-java@99b8673ff64fbf99d8d325f52d9a5bdedb8483e9 # v4.2.1',
+				with: {
+					distribution: 'temurin',
+					'java-version': '17',
+				},
+			},
+			{
+				name: 'Submit dependencies',
+				id: 'submit',
+				uses: 'gradle/actions/dependency-submission@d9c87d481d55275bb5441eef3fe0e46805f9ef70 # v3.5.0',
+			},
+			{
+				name: 'Log snapshot for user validation',
+				id: 'validate',
+				run: `cat /home/runner/work/${repo}/${repo}/dependency-graph-reports/update_dependency_graph_for_kotlin-dependency-graph.json | jq`,
+			},
+		],
+	};
+}
 
 export function createYaml(
 	prBranch: string,
 	language: DepGraphLanguage,
+	repo: string,
 ): string {
 	const dependencyGraphWorkflowJson = {
 		name: `Update Dependency Graph for ${language}`,
@@ -25,30 +69,14 @@ export function createYaml(
 		jobs: {
 			'dependency-graph': {
 				'runs-on': 'ubuntu-latest',
-				steps: [
-					{
-						name: 'Checkout branch',
-						id: 'checkout',
-						uses: 'actions/checkout@692973e3d937129bcbf40652eb9f2f61becf3332 # v4.1.7',
-					},
-					{
-						name: 'Submit dependencies',
-						id: 'submit',
-						uses: workflowsForLanguages[`${language}`],
-					},
-					{
-						name: 'Log snapshot for user validation',
-						id: 'validate',
-						run: 'cat ${{ steps.submit.outputs.snapshot-json-path }} | jq',
-					},
-				],
+				steps: createYamlLanguageSteps(repo)[language],
 				permissions: { contents: 'write' },
 			},
 		},
 	};
 
 	return stringify(dependencyGraphWorkflowJson, { lineWidth: 120 })
-		.replace('{}', '')
+		.replaceAll('{}', '')
 		.replaceAll('"', '');
 }
 
@@ -56,14 +84,16 @@ const stepsForLanguages: Record<DepGraphLanguage, string[]> = {
 	Scala: [
 		'Ensure that the [version of sbt in the project is v1.5 or above](https://github.com/scalacenter/sbt-dependency-submission?tab=readme-ov-file#support) in order for the dependency submission action to run.',
 	],
-	Kotlin: [],
+	Kotlin: [
+		'Ensure that the [version of Gradle is v5.2 or above](https://github.com/gradle/actions/blob/main/docs/dependency-submission.md#gradle-version-compatibility)',
+	],
 };
 
 const languageSpecificInfo: Record<DepGraphLanguage, string> = {
 	Scala:
 		'See [the SBT workflow documentation](https://github.com/scalacenter/sbt-dependency-submission?tab=readme-ov-file) for further information and configuration options.',
 	Kotlin:
-		'See [the Gradle workflow documentation](https://github.com/gradle/actions/blob/main/docs/dependency-submission.md) for further information and configuration options.',
+		'See [the Gradle workflow documentation](https://github.com/gradle/actions/blob/main/docs/dependency-submission.md) for further information and configuration options, and the [FAQ for troubleshooting](https://github.com/gradle/actions/blob/main/docs/dependency-submission-faq.md).',
 };
 
 function createPRChecklist(
