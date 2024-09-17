959ee189e899b52d146cd124c1b3a081e9bc8224
Natasha <67543397+NovemberTang@users.noreply.github.com>
2024-09-17 21:47:30 +0100

    make gitHubOrg a prop

--- packages/cdk/lib/service-catalogue.ts
+++ packages/cdk/lib/service-catalogue.ts
@@ -62,6 +62,11 @@ interface ServiceCatalogueProps extends GuStackProps {
 	 */
 	rdsDeletionProtection?: boolean;
 	multiAz?: boolean;
+
+	/**
+	 * The GitHub org to search for repositories in.
+	 */
+	gitHubOrg?: string;
 }
 
 export class ServiceCatalogue extends GuStack {
@@ -71,7 +76,11 @@ export class ServiceCatalogue extends GuStack {
 		const { stage, stack } = this;
 		const app = props.app ?? 'service-catalogue';
 
-		const { rdsDeletionProtection = true, multiAz = false } = props;
+		const {
+			rdsDeletionProtection = true,
+			multiAz = false,
+			gitHubOrg = 'guardian',
+		} = props;
 
 		const nonProdSchedule = props.schedule;
 
@@ -172,8 +181,6 @@ export class ServiceCatalogue extends GuStack {
 			effect: Effect.ALLOW,
 			resources: [loggingStreamArn],
 		});
-
-		const gitHubOrg = 'guardian';
 
 		const cloudqueryCluster = addCloudqueryEcsCluster(this, {
 			nonProdSchedule,
