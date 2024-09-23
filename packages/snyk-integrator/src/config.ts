d5f2ac29083b46625bddf25dc0f360e60e316476
Natasha <67543397+NovemberTang@users.noreply.github.com>
2024-09-23 16:58:52 +0100

    use snyk integrator config to set org name, use common PR raising functions in dependency-graph-integrator

--- packages/snyk-integrator/src/config.ts
+++ packages/snyk-integrator/src/config.ts
@@ -5,10 +5,16 @@ export interface Config {
 	 * The stage of the application, e.g. DEV, CODE, PROD.
 	 */
 	stage: string;
+
+	/**
+	 * The GitHub org to use
+	 */
+	owner: string;
 }
 
 export function getConfig(): Config {
 	return {
 		stage: getEnvOrThrow('STAGE'),
+		owner: process.env['GITHUB_ORG'] ?? 'guardian',
 	};
 }
