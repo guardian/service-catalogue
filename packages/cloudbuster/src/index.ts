b42f4a304f67349fde935dfa3785f5940c3fff1b
Natasha <67543397+NovemberTang@users.noreply.github.com>
2024-09-24 10:35:03 +0100

    test findingsToGuardianFormat

--- packages/cloudbuster/src/index.ts
+++ packages/cloudbuster/src/index.ts
@@ -1,45 +1,14 @@
 import { Anghammarad } from '@guardian/anghammarad';
-import type { cloudbuster_fsbp_vulnerabilities } from '@prisma/client';
-import { isWithinSlaTime, stringToSeverity } from 'common/functions';
 import { getFsbpFindings } from 'common/src/database-queries';
 import { getPrismaClient } from 'common/src/database-setup';
-import type { SecurityHubFinding, SecurityHubSeverity } from 'common/src/types';
+import type { SecurityHubSeverity } from 'common/src/types';
 import { getConfig } from './config';
 import { createDigestsFromFindings, sendDigest } from './digests';
-import { transformFinding } from './findings';
+import { findingsToGuardianFormat, transformFinding } from './findings';
 
 type LambdaHandlerProps = {
 	severities?: SecurityHubSeverity[];
 };
-
-function findingsToGuardianFormat(
-	finding: SecurityHubFinding,
-): cloudbuster_fsbp_vulnerabilities[] {
-	const transformedFindings: cloudbuster_fsbp_vulnerabilities[] =
-		finding.resources.map((r) => {
-			const guFinding: cloudbuster_fsbp_vulnerabilities = {
-				severity: finding.severity.Label,
-				control_id: finding.product_fields.ControlId,
-				title: finding.title,
-				aws_region: r.Region,
-				repo: r.Tags?.['gu:repo'] ?? null,
-				stack: r.Tags?.['Stack'] ?? null,
-				stage: r.Tags?.Stage ?? null,
-				app: r.Tags?.App ?? null,
-				first_observed_at: finding.first_observed_at,
-				arn: r.Id,
-				aws_account_name: finding.aws_account_name,
-				aws_account_id: finding.aws_account_id,
-				within_sla: isWithinSlaTime(
-					finding.first_observed_at,
-					stringToSeverity(finding.severity.Label),
-				),
-				remediation: finding.remediation.Recommendation.Url,
-			};
-			return guFinding;
-		});
-	return transformedFindings;
-}
 
 export async function main(input: LambdaHandlerProps) {
 	// When manually invoking the function in AWS for testing,
