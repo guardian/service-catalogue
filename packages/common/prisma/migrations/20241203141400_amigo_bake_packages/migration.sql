4603ab86fcb98d6d45765de852cfdcfb1af57739
Kelvin Chappell <kelvin.chappell@theguardian.com>
2024-12-04 09:13:41 +0000

    Make packages baked into images by Amigo visible to the service catalogue (#1333)
    * Add CDK config for image package source
    
    * Add new table to Prisma schema
    
    * Add table migration script

---
+++ packages/common/prisma/migrations/20241203141400_amigo_bake_packages/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "amigo_bake_packages"
+(
+    "_cq_sync_time"   TIMESTAMP(6),
+    "_cq_source_name" TEXT,
+    "_cq_id"          UUID      NOT NULL,
+    "_cq_parent_id"   UUID,
+    "base_name"       TEXT      NOT NULL,
+    "base_ami_id"     TEXT      NOT NULL,
+    "base_eol_date"   TIMESTAMP NOT NULL,
+    "recipe_id"       TEXT      NOT NULL,
+    "bake_number"     INTEGER   NOT NULL,
+    "source_ami_id"   TEXT      NOT NULL,
+    "started_at"      TIMESTAMP NOT NULL,
+    "started_by"      TEXT      NOT NULL,
+    "package_name"    TEXT      NOT NULL,
+    "package_version" TEXT      NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "amigo_bake_packages__cq_id_key" ON "amigo_bake_packages" ("_cq_id");
