2b134c7239d9577b4e9ea5c05a7d853fe275ebcf
Natasha <67543397+NovemberTang@users.noreply.github.com>
2024-12-11 06:44:14 +0000

    pass in the github org name via config (#1310)
    Co-authored-by: TJ Silver <15648334+tjsilver@users.noreply.github.com>

--- packages/dependency-graph-integrator/src/config.ts
+++ packages/dependency-graph-integrator/src/config.ts
@@ -5,10 +5,16 @@ export interface Config {
 	 * The stage of the application, e.g. DEV, CODE, PROD.
 	 */
 	stage: string;
+
+	/**
+	 * The GitHub organization to use.
+	 */
+	gitHubOrg: string;
 }
 
 export function getConfig(): Config {
 	return {
 		stage: getEnvOrThrow('STAGE'),
+		gitHubOrg: process.env.GITHUB_ORG ?? 'guardian',
 	};
 }
--- packages/dependency-graph-integrator/src/repo-functions.ts
+++ packages/dependency-graph-integrator/src/repo-functions.ts
@@ -1,19 +1,18 @@
 import type { Octokit } from 'octokit';
 import type { StatusCode } from './types';
 
-const OWNER = 'guardian';
-
 const ghHeaders = { 'X-GitHub-Api-Version': '2022-11-28' };
 
 export async function enableDependabotAlerts(
 	repo: string,
 	octokit: Octokit,
+	owner: string,
 ): Promise<StatusCode> {
 	console.log(`Enabling Dependabot alerts for ${repo}`);
 	const enableResponse = await octokit.request(
 		'PUT /repos/{owner}/{repo}/vulnerability-alerts',
 		{
-			owner: OWNER,
+			owner,
 			repo,
 			headers: ghHeaders,
 		},
