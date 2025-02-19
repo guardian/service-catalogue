7a7795db18387dbfebf6468a37ec6438e8842c01
TJ Silver <thalia.silver@guardian.co.uk>
2025-02-19 11:21:09 +0000

    refactor: remove Snyk tables from frequency table

--- packages/common/prisma/migrations/20250218105625_remove_snyk_tables/migration.sql
+++ packages/common/prisma/migrations/20250218105625_remove_snyk_tables/migration.sql
@@ -1,12 +1,18 @@
--- DropTable
-DROP TABLE IF EXISTS "snyk_issues";
+BEGIN;
 
--- DropTable
-DROP TABLE IF EXISTS "snyk_projects";
+-- Remove the frequency data for deprecated Snyk tables else alarms will trip
+DELETE FROM cloudquery_table_frequency
+WHERE table_name IN (
+    'snyk_issues'
+    , 'snyk_projects'
+    , 'snyk_sbom'
+    , 'snyk_organizations'
+);
 
--- DropTable
-DROP TABLE IF EXISTS "snyk_sbom";
+-- Remove deprecated Snyk tables
+DROP TABLE IF EXISTS snyk_issues;
+DROP TABLE IF EXISTS snyk_projects;
+DROP TABLE IF EXISTS snyk_sbom;
+DROP TABLE IF EXISTS snyk_organizations;
 
--- DropTable
-DROP TABLE IF EXISTS "snyk_organizations";
- 
+COMMIT;
