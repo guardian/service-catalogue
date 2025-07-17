e680926d06febdaa49cf7ac515b698fabd63b638
TJ Silver <thalia.silver@guardian.co.uk>
2025-07-17 09:16:11 +0100

    refactor(tests): remove unnecessary configuration from Vitest setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,18 +3,8 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    setupFiles: ['dotenv/config', '../../vitest.setup.ts'],
-    clearMocks: true,
     reporters: process.env.GITHUB_ACTIONS 
       ? ['verbose', 'github-actions'] 
       : ['verbose'],
-    server: {
-      deps: {
-        external: [
-          '@guardian/cdk/lib/constants/tracking-tag',
-          '@guardian/private-infrastructure-config'
-        ]
-      }
-    }
   }
 });
