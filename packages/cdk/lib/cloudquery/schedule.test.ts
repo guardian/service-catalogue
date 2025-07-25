87310fe7a5b45d86ced76b642f8fa5f460531f68
TJ Silver <thalia.silver@guardian.co.uk>
2025-07-25 17:55:15 +0100

    refactor(wip): update test files to use ESM imports and improve readability

--- packages/cdk/lib/cloudquery/schedule.test.ts
+++ packages/cdk/lib/cloudquery/schedule.test.ts
@@ -1,51 +1,51 @@
-// import { Duration } from 'aws-cdk-lib';
-// import { Schedule } from 'aws-cdk-lib/aws-events';
-// import { scheduleFrequencyMs } from './schedule';
+import { Duration } from 'aws-cdk-lib';
+import { Schedule } from 'aws-cdk-lib/aws-events';
+import { scheduleFrequencyMs } from './schedule.js';
 
-// describe('EventBridge expression parsing', () => {
-// 	it('should correctly identify task frequency from CRON', () => {
-// 		expect(
-// 			scheduleFrequencyMs(
-// 				Schedule.cron({
-// 					hour: '1',
-// 				}),
-// 			),
-// 		).toBe(Duration.minutes(1).toMilliseconds());
-// 		expect(
-// 			scheduleFrequencyMs(
-// 				Schedule.cron({
-// 					minute: '1',
-// 					hour: '1',
-// 					weekDay: '1',
-// 				}),
-// 			),
-// 		).toBe(Duration.days(7).toMilliseconds());
-// 	});
+describe('EventBridge expression parsing', () => {
+	it('should correctly identify task frequency from CRON', () => {
+		expect(
+			scheduleFrequencyMs(
+				Schedule.cron({
+					hour: '1',
+				}),
+			),
+		).toBe(Duration.minutes(1).toMilliseconds());
+		expect(
+			scheduleFrequencyMs(
+				Schedule.cron({
+					minute: '1',
+					hour: '1',
+					weekDay: '1',
+				}),
+			),
+		).toBe(Duration.days(7).toMilliseconds());
+	});
 
-// 	it('should correctly identify task frequency from RATE', () => {
-// 		expect(scheduleFrequencyMs(Schedule.rate(Duration.days(1)))).toBe(
-// 			Duration.days(1).toMilliseconds(),
-// 		);
-// 		expect(scheduleFrequencyMs(Schedule.rate(Duration.days(7)))).toBe(
-// 			Duration.days(7).toMilliseconds(),
-// 		);
-// 		expect(scheduleFrequencyMs(Schedule.rate(Duration.days(30)))).toBe(
-// 			Duration.days(30).toMilliseconds(),
-// 		);
-// 	});
+	it('should correctly identify task frequency from RATE', () => {
+		expect(scheduleFrequencyMs(Schedule.rate(Duration.days(1)))).toBe(
+			Duration.days(1).toMilliseconds(),
+		);
+		expect(scheduleFrequencyMs(Schedule.rate(Duration.days(7)))).toBe(
+			Duration.days(7).toMilliseconds(),
+		);
+		expect(scheduleFrequencyMs(Schedule.rate(Duration.days(30)))).toBe(
+			Duration.days(30).toMilliseconds(),
+		);
+	});
 
-// 	it('should correctly identify task frequency from EXPRESSION', () => {
-// 		expect(scheduleFrequencyMs(Schedule.expression('rate(1 days)'))).toBe(
-// 			Duration.days(1).toMilliseconds(),
-// 		);
-// 	});
+	it('should correctly identify task frequency from EXPRESSION', () => {
+		expect(scheduleFrequencyMs(Schedule.expression('rate(1 days)'))).toBe(
+			Duration.days(1).toMilliseconds(),
+		);
+	});
 
-// 	it('should throw error from invalid EXPRESSION', () => {
-// 		expect(() => scheduleFrequencyMs(Schedule.expression('asdf'))).toThrow(
-// 			Error,
-// 		);
-// 		expect(() =>
-// 			scheduleFrequencyMs(Schedule.expression('asdf(asdf)')),
-// 		).toThrow(Error);
-// 	});
-// });
+	it('should throw error from invalid EXPRESSION', () => {
+		expect(() => scheduleFrequencyMs(Schedule.expression('asdf'))).toThrow(
+			Error,
+		);
+		expect(() =>
+			scheduleFrequencyMs(Schedule.expression('asdf(asdf)')),
+		).toThrow(Error);
+	});
+});
