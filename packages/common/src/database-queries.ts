c4e2394e0f2c13c4a2947f3b1d4407870b6ec252
Natasha <67543397+NovemberTang@users.noreply.github.com>
2025-08-08 10:10:21 +0100

    move getRepositories to common

--- packages/common/src/database-queries.ts
+++ packages/common/src/database-queries.ts
@@ -1,5 +1,6 @@
 import type { aws_securityhub_findings, PrismaClient } from '@prisma/client';
-import type { SecurityHubFinding, SecurityHubSeverity } from './types.js';
+import { toNonEmptyArray } from './functions.js';
+import type { Repository, SecurityHubFinding, SecurityHubSeverity } from './types.js';
 /**
  * Queries the database for FSBP findings
  */
@@ -23,4 +24,25 @@ export async function getFsbpFindings(
 		});
 
 	return findings as unknown as SecurityHubFinding[];
+}
+
+export async function getRepositories(
+	client: PrismaClient,
+	ignoredRepositoryPrefixes: string[],
+): Promise<Repository[]> {
+	console.debug('Discovering repositories');
+	const repositories = await client.github_repositories.findMany({
+		where: {
+			NOT: [
+				{
+					OR: ignoredRepositoryPrefixes.map((prefix) => {
+						return { full_name: { startsWith: prefix } };
+					}),
+				},
+			],
+		},
+	});
+
+	console.debug(`Found ${repositories.length} repositories`);
+	return toNonEmptyArray(repositories.map((r) => r as Repository));
 }
