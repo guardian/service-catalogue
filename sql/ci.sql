229e1faa818bef399888620f664b99d36666a807
akash1810 <akash1810@users.noreply.github.com>
2025-09-17 06:31:31 +0100

    feat!(postgres): Drop `audit_results` table
    Drops the `audit_results` table and removes privileges to the `dataaudit` user.
    Attempting to drop the user highlights some permissions issues:
    
    ```
    permission denied to drop objects
    ```

--- sql/ci.sql
+++ sql/ci.sql
@@ -55,33 +55,6 @@ VALUES (
 DELETE FROM cloudbuster_fsbp_vulnerabilities
 WHERE arn = 'arn:aws:securityhub:eu-west-1:123456789012:product/aws/securityhub/finding/12345678901234567890123456789012';
 
-
--- Switch to the `dataaudit` user and test access to the tables/views used in the data-audit app
-SET ROLE dataaudit;
--- It should be able to read from these tables
-SELECT * FROM aws_s3_buckets LIMIT 1;
-SELECT * FROM aws_lambda_functions LIMIT 1;
-SELECT * FROM aws_accounts LIMIT 1;
-
--- It should be able to read/write from the table audit_results
-INSERT INTO audit_results (
-    evaluated_on
-    , name
-    , success
-    , cloudquery_total
-    , vendor_total
-) VALUES (
-    NOW()
-    , 'test'
-    , TRUE
-    , 1
-    , 1
-);
-SELECT * FROM audit_results LIMIT 1;
-
-DELETE FROM audit_results
-WHERE name = 'test';
-
 -- The user github_actions_usage...
 SET ROLE github_actions_usage;
 
