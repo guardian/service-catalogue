d62d565230b72205163d006c34fdb6a75bfaa43f
TJ Silver <thalia.silver@guardian.co.uk>
2025-11-20 14:46:22 +0000

    test: add more tests

--- packages/repocop/src/remediation/branch-protector/branch-protection.test.ts
+++ packages/repocop/src/remediation/branch-protector/branch-protection.test.ts
@@ -22,8 +22,6 @@ void describe('Team slugs should be findable for every team associated with a re
 		const teamOneOwner: view_repo_ownership = {
 			...nullOwner,
 			full_repo_name: repo,
-			github_team_id: BigInt(1),
-			github_team_name: 'Team One',
 			github_team_slug: 'team-one',
 		};
 
@@ -38,6 +36,77 @@ void describe('Team slugs should be findable for every team associated with a re
 		const repo = 'guardian/repo1';
 		const actual = createBranchProtectionEvents([repo], []);
 
+		assert.strictEqual(actual.length, 0);
+	});
+
+	void test('Multiple repositories with different owners should each get their own event', () => {
+		const repo1 = 'guardian/repo1';
+		const repo2 = 'guardian/repo2';
+
+		const teamOneOwner: view_repo_ownership = {
+			...nullOwner,
+			full_repo_name: repo1,
+			github_team_slug: 'team-alpha',
+		};
+
+		const teamTwoOwner: view_repo_ownership = {
+			...nullOwner,
+			full_repo_name: repo2,
+			github_team_slug: 'team-beta',
+		};
+
+		const actual = createBranchProtectionEvents(
+			[repo1, repo2],
+			[teamOneOwner, teamTwoOwner],
+		);
+
+		assert.strictEqual(actual.length, 2);
+		assert.deepStrictEqual(actual[0], {
+			fullName: repo1,
+			teamNameSlugs: ['team-alpha'],
+		});
+		assert.deepStrictEqual(actual[1], {
+			fullName: repo2,
+			teamNameSlugs: ['team-beta'],
+		});
+	});
+
+	void test('A repository with multiple team owners should have all team slugs in one event', () => {
+		const repo = 'guardian/multi-owner-repo';
+
+		const teamAlphaOwner: view_repo_ownership = {
+			...nullOwner,
+			full_repo_name: repo,
+			github_team_slug: 'team-alpha',
+		};
+
+		const teamBetaOwner: view_repo_ownership = {
+			...nullOwner,
+			full_repo_name: repo,
+			github_team_slug: 'team-beta',
+		};
+
+		const actual = createBranchProtectionEvents(
+			[repo],
+			[teamAlphaOwner, teamBetaOwner],
+		);
+
+		assert.strictEqual(actual.length, 1);
+		assert.ok(actual[0]);
+		assert.strictEqual(actual[0].fullName, repo);
+		assert.strictEqual(actual[0].teamNameSlugs.length, 2);
+		assert.ok(actual[0].teamNameSlugs.includes('team-alpha'));
+		assert.ok(actual[0].teamNameSlugs.includes('team-beta'));
+	});
+
+	void test('Empty repository list should return empty events', () => {
+		const teamOwner: view_repo_ownership = {
+			...nullOwner,
+			full_repo_name: 'guardian/some-repo',
+			github_team_slug: 'team-one',
+		};
+
+		const actual = createBranchProtectionEvents([], [teamOwner]);
 		assert.strictEqual(actual.length, 0);
 	});
 });
