685b1cf20d0f2bcbafacc808d938d7b6144d8451
Natasha <67543397+NovemberTang@users.noreply.github.com>
2026-02-04 13:34:32 +0000

    get all PRs for one repo

--- packages/repocop/src/types.ts
+++ packages/repocop/src/types.ts
@@ -45,18 +45,6 @@ export type DependabotVulnResponse =
 
 export type Alert = DependabotVulnResponse['data'][number];
 
-export type VulnerabilityAlertPRResponse = {
-	repository: {
-		vulnerabilityAlert: {
-			dependabotUpdate: {
-				pullRequest: {
-					url: string | null;
-				} | null;
-			} | null;
-		};
-	};
-};
-
 export interface RepoAndAlerts {
 	shortName: string;
 	/*
