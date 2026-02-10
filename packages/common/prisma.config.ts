5818769b5e4e4e2fcaceaa96b60ad555ba3bea19
Natasha <67543397+NovemberTang@users.noreply.github.com>
2026-02-10 14:57:37 +0000

    attempt to fix CI job

--- packages/common/prisma.config.ts
+++ packages/common/prisma.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
 	schema: path.join('prisma', 'schema.prisma'),
+	datasource: {
+		url: { fromEnvVar: 'DATABASE_URL' },
+	},
 	migrations: {
 		path: path.join('prisma', 'migrations'),
 	},
