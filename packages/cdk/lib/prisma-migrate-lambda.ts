e27d93a12cd2545de45d803514cbe200aacc8f43
TJ Silver <thalia.silver@guardian.co.uk>
2026-04-13 16:41:03 +0100

    fix: update createPrismaMigrateLambda to accept DatabaseInstance and set DATABASE_URL

--- packages/cdk/lib/prisma-migrate-lambda.ts
+++ packages/cdk/lib/prisma-migrate-lambda.ts
@@ -1,13 +1,24 @@
 import type { GuStack } from '@guardian/cdk/lib/constructs/core';
 import { GuLambdaFunction } from '@guardian/cdk/lib/constructs/lambda';
 import { Architecture, Runtime } from 'aws-cdk-lib/aws-lambda';
+import type { DatabaseInstance } from 'aws-cdk-lib/aws-rds';
 
-export function createPrismaMigrateLambda(scope: GuStack) {
-	return new GuLambdaFunction(scope, 'prisma-migrate', {
+export function createPrismaMigrateLambda(
+	scope: GuStack,
+	db: DatabaseInstance,
+) {
+	const fn = new GuLambdaFunction(scope, 'prisma-migrate', {
 		app: 'prisma-migrate',
 		architecture: Architecture.ARM_64,
 		runtime: Runtime.NODEJS_20_X,
 		fileName: 'prisma.zip',
 		handler: 'dist/src/prisma-migrate.main',
+		environment: {
+			DATABASE_URL: db.secret!.secretValueFromJson('url').toString(),
+		},
 	});
+
+	db.secret?.grantRead(fn);
+
+	return fn;
 }
