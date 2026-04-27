a8f5ec1bcd0dea18f1691e512ef8b90122641767
Natasha <67543397+NovemberTang@users.noreply.github.com>
2026-04-27 09:02:13 +0000

    install tsx

--- packages/common/prisma.config.ts
+++ packages/common/prisma.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
 	},
 	migrations: {
 		path: path.join('prisma', 'migrations'),
-		seed: "npx tsx prisma/seed.ts",
+		seed: "tsx prisma/seed.ts",
 	},
 	views: {
 		path: path.join('prisma', 'views'),
