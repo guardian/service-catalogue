6defd5d840e7606982a8f47a955b9c1da63dc7fc
akash1810 <akash1810@users.noreply.github.com>
2026-05-22 11:45:40 +0100

    feat: Collect `aws_ecs_*` tables in `AwsResourcesForAvailabilityDashboard`

--- packages/cdk/lib/cloudquery/index.ts
+++ packages/cdk/lib/cloudquery/index.ts
@@ -220,7 +220,11 @@ export function addCloudqueryEcsCluster(
 			description: 'Resources queried by the Availability dashboard',
 			schedule: Schedule.rate(Duration.minutes(30)),
 			config: awsSourceConfigForOrganisation({
-				tables: filterCloudQueryTables([/^aws_elbv1_.*$/, /^aws_elbv2_.*$/]),
+				tables: filterCloudQueryTables([
+					/^aws_elbv1_.*$/,
+					/^aws_elbv2_.*$/,
+					/^aws_ecs_.*$/,
+				]),
 			}),
 			policies: [listOrgsPolicy, cloudqueryAccess('*')],
 		},
