79552b19f0daf511a790625428c03745f4a4639e
TJ Silver <thalia.silver@guardian.co.uk>
2026-06-01 16:10:11 +0100

    refactor: add non_null constraint to alert_type query

--- packages/common/prisma/migrations/20260527164433_repocop_vulnerabilities_classification_column/migration.sql
+++ packages/common/prisma/migrations/20260527164433_repocop_vulnerabilities_classification_column/migration.sql
@@ -1,5 +1,19 @@
 -- Add alert_type column to repocop_vulnerabilities table. This will enable us to differentiate Dependabot malware alerts from vulnerability alerts.
 
+BEGIN;
+
 ALTER TABLE IF EXISTS repocop_vulnerabilities
   ADD COLUMN IF NOT EXISTS alert_type text;
 
+UPDATE repocop_vulnerabilities
+SET alert_type = 'general'
+WHERE alert_type IS NULL;
+
+ALTER TABLE repocop_vulnerabilities
+  ALTER COLUMN alert_type SET NOT NULL;
+
+ALTER TABLE repocop_vulnerabilities
+  ADD CONSTRAINT repocop_vulnerabilities_alert_type_check
+  CHECK (alert_type IN ('general', 'malware'));
+
+COMMIT;
