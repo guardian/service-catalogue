883e6a7c4a6375b3185541b1d8206a4fa46e599b
Natasha <67543397+NovemberTang@users.noreply.github.com>
2026-07-06 09:55:24 +0000

    grant all table read to cloudbuster

---
+++ packages/common/prisma/migrations/20260706105205_cloudbuster_read/migration.sql
@@ -0,0 +1,8 @@
+-- makes cloudbuster consistent with other DB users in 20240709104714_user_permissions
+BEGIN TRANSACTION;
+    -- Grant read access to all current tables, and views
+    GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudbuster;
+
+    -- Grant read access to all future tables, and views
+        ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO cloudbuster;
+COMMIT TRANSACTION;
