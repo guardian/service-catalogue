0023076cf935e6687da9633cebccb68675f917f6
Natasha <67543397+NovemberTang@users.noreply.github.com>
2026-09-04 12:49:03 +0000

    set concurrency to 1

--- packages/cdk/lib/cloudquery/index.ts
+++ packages/cdk/lib/cloudquery/index.ts
@@ -454,7 +454,7 @@ export function addCloudqueryEcsCluster(
 					'github_repository_custom_properties',
 					'github_workflows',
 				],
-				concurrency: 10,
+				concurrency: 1,
 			}),
 			secrets: githubSecrets,
 			additionalCommands: additionalGithubCommands,
