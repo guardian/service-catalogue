469fcd6f8fc40817e1abc0281af9c4f090d2723d
Natasha <67543397+NovemberTang@users.noreply.github.com>
2026-09-04 13:25:39 +0000

    back to 10

--- packages/cdk/lib/cloudquery/index.ts
+++ packages/cdk/lib/cloudquery/index.ts
@@ -454,7 +454,7 @@ export function addCloudqueryEcsCluster(
 					'github_repository_custom_properties',
 					'github_workflows',
 				],
-				concurrency: 1,
+				concurrency: 10,
 			}),
 			secrets: githubSecrets,
 			additionalCommands: additionalGithubCommands,
